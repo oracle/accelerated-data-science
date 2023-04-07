345f8b5528bfd7007a0f2f75f81120482097dc3e
Dmitrii Cherkasov <dmitrii.cherkasov@oracle.com>
2023-04-07 13:18:12 -0700

    ODSC-41193: Adds API section to the telemetry.

--- tests/unitary/default_setup/telemetry/test_agent.py
+++ tests/unitary/default_setup/telemetry/test_agent.py
@@ -3,12 +3,19 @@
 # Copyright (c) 2022, 2023 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 
-from unittest.mock import patch
-import pytest
-from pytest import MonkeyPatch
 import importlib
+from unittest.mock import patch
+
+import pytest
+# from pytest import MonkeyPatch
 
 import ads
+from ads.telemetry.telemetry import (
+    EXTRA_USER_AGENT_INFO,
+    LIBRARY,
+    UNKNOWN,
+    USER_AGENT_KEY,
+)
 
 
 class TestUserAgent:
@@ -31,7 +38,7 @@ class TestUserAgent:
             auth_info = ads.auth.api_keys("test_path", "TEST_PROFILE")
             assert (
                 auth_info["config"].get("additional_user_agent")
-                == f"Oracle-ads/version={ads.__version__}/surface=WORKSTATION"
+                == f"{LIBRARY}/version={ads.__version__}/surface=WORKSTATION/api={UNKNOWN}"
             )
 
     @patch("oci.auth.signers.get_resource_principals_signer")
@@ -41,37 +48,66 @@ class TestUserAgent:
         auth_info = ads.auth.resource_principal()
         assert (
             auth_info["config"].get("additional_user_agent")
-            == f"Oracle-ads/version={ads.__version__}/surface=WORKSTATION"
+            == f"{LIBRARY}/version={ads.__version__}/surface=WORKSTATION/api={UNKNOWN}"
         )
 
     @patch("oci.signer.load_private_key_from_file")
-    def test_user_agent_default_signer(self, mock_load_key_file):
-        monkeypatch = MonkeyPatch()
+    def test_user_agent_default_signer(self, mock_load_key_file, monkeypatch):
+        # monkeypatch = MonkeyPatch()
         monkeypatch.delenv("OCI_RESOURCE_PRINCIPAL_VERSION", raising=False)
         importlib.reload(ads.config)
         with patch("oci.config.from_file", return_value=self.test_config):
             auth_info = ads.auth.default_signer()
             assert (
                 auth_info["config"].get("additional_user_agent")
-                == f"Oracle-ads/version={ads.__version__}/surface=WORKSTATION"
+                == f"{LIBRARY}/version={ads.__version__}/surface=WORKSTATION/api={UNKNOWN}"
             )
 
     @pytest.mark.parametrize(
-        "RESOURCE_KEY, USER_AGENT_VALUE",
+        "INPUT_DATA, EXPECTED_RESULT",
         [
-            ("MD_OCID", "DATASCIENCE_MODEL_DEPLOYMENT"),
-            ("JOB_RUN_OCID", "DATASCIENCE_JOB"),
-            ("NB_SESSION_OCID", "DATASCIENCE_NOTEBOOK"),
-            ("DATAFLOW_RUN_ID", "DATAFLOW"),
+            (
+                {"RESOURCE_KEY": "MD_OCID", EXTRA_USER_AGENT_INFO: ""},
+                {
+                    "USER_AGENT_VALUE": "DATASCIENCE_MODEL_DEPLOYMENT",
+                    EXTRA_USER_AGENT_INFO: UNKNOWN,
+                },
+            ),
+            (
+                {"RESOURCE_KEY": "JOB_RUN_OCID", EXTRA_USER_AGENT_INFO: "test_api"},
+                {
+                    "USER_AGENT_VALUE": "DATASCIENCE_JOB",
+                    EXTRA_USER_AGENT_INFO: "test_api",
+                },
+            ),
+            (
+                {"RESOURCE_KEY": "NB_SESSION_OCID", EXTRA_USER_AGENT_INFO: None},
+                {
+                    "USER_AGENT_VALUE": "DATASCIENCE_NOTEBOOK",
+                    EXTRA_USER_AGENT_INFO: UNKNOWN,
+                },
+            ),
+            (
+                {
+                    "RESOURCE_KEY": "DATAFLOW_RUN_ID",
+                    EXTRA_USER_AGENT_INFO: "some_class&some_method",
+                },
+                {
+                    "USER_AGENT_VALUE": "DATAFLOW",
+                    EXTRA_USER_AGENT_INFO: "some_class&some_method",
+                },
+            ),
         ],
     )
     @patch("oci.signer.load_private_key_from_file")
     def test_user_agent_default_signer_known_resources(
-        self, mock_load_key_file, RESOURCE_KEY, USER_AGENT_VALUE
+        self,mock_load_key_file, monkeypatch, INPUT_DATA, EXPECTED_RESULT
     ):
-        monkeypatch = MonkeyPatch()
+        # monkeypatch = MonkeyPatch()
         monkeypatch.setenv("OCI_RESOURCE_PRINCIPAL_VERSION", "1.1")
-        monkeypatch.setenv(RESOURCE_KEY, "1234")
+        monkeypatch.setenv(INPUT_DATA["RESOURCE_KEY"], "1234")
+        if INPUT_DATA[EXTRA_USER_AGENT_INFO] is not None:
+            monkeypatch.setenv(EXTRA_USER_AGENT_INFO, INPUT_DATA[EXTRA_USER_AGENT_INFO])
 
         importlib.reload(ads.config)
         importlib.reload(ads)
@@ -82,17 +118,20 @@ class TestUserAgent:
             auth_info = ads.auth.default_signer()
             assert (
                 auth_info["config"].get("additional_user_agent")
-                == f"Oracle-ads/version={ads.__version__}/surface={USER_AGENT_VALUE}"
+                == f"{LIBRARY}/version={ads.__version__}/surface={EXPECTED_RESULT['USER_AGENT_VALUE']}/api={EXPECTED_RESULT[EXTRA_USER_AGENT_INFO]}"
             )
         monkeypatch.delenv("OCI_RESOURCE_PRINCIPAL_VERSION", raising=False)
-        monkeypatch.delenv(RESOURCE_KEY, raising=False)
+        monkeypatch.delenv(INPUT_DATA["RESOURCE_KEY"], raising=False)
+        monkeypatch.delenv(EXTRA_USER_AGENT_INFO, raising=False)
 
     @patch("oci.signer.Signer")
     def test_user_agent_default_singer_ociservice(
         self,
         mock_signer,
+        monkeypatch
     ):
-        monkeypatch = MonkeyPatch()
+        #monkeypatch = MonkeyPatch()
+        monkeypatch.setenv("OCI_RESOURCE_PRINCIPAL_VERSION", "1.1")
         monkeypatch.setenv("OCI_RESOURCE_PRINCIPAL_VERSION", "1.1")
 
         importlib.reload(ads.config)
@@ -102,6 +141,6 @@ class TestUserAgent:
             auth_info = ads.auth.default_signer()
             assert (
                 auth_info["config"].get("additional_user_agent")
-                == f"Oracle-ads/version={ads.__version__}/surface=OCI_SERVICE"
+                == f"{LIBRARY}/version={ads.__version__}/surface=OCI_SERVICE/api={UNKNOWN}"
             )
         monkeypatch.delenv("OCI_RESOURCE_PRINCIPAL_VERSION", raising=False)
