eb810ebebac235f8ecc93adcd48393d0c637bcb5
Lu Peng <bolu.peng@oracle.com>
2023-05-12 16:07:04 -0700

    Fixed unit test in other files.

--- tests/unitary/default_setup/telemetry/test_agent.py
+++ tests/unitary/default_setup/telemetry/test_agent.py
@@ -4,6 +4,7 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 
 import importlib
+import oci
 from unittest.mock import patch
 
 import pytest
@@ -34,12 +35,13 @@ class TestUserAgent:
 
     @patch("oci.signer.Signer")
     def test_user_agent_api_keys_using_test_profile(self, mock_signer):
-        with patch("oci.config.from_file", return_value=self.test_config):
-            auth_info = ads.auth.api_keys("test_path", "TEST_PROFILE")
-            assert (
-                auth_info["config"].get("additional_user_agent")
-                == f"{LIBRARY}/version={ads.__version__}#surface=WORKSTATION#api={UNKNOWN}"
-            )
+        with pytest.raises(oci.exceptions.InvalidConfig):
+            with patch("oci.config.from_file", return_value=self.test_config):
+                auth_info = ads.auth.api_keys("test_path", "TEST_PROFILE")
+                assert (
+                    auth_info["config"].get("additional_user_agent")
+                    == f"{LIBRARY}/version={ads.__version__}#surface=WORKSTATION#api={UNKNOWN}"
+                )
 
     @patch("oci.auth.signers.get_resource_principals_signer")
     def test_user_agent_rp(self, mock_signer, monkeypatch):
@@ -54,15 +56,16 @@ class TestUserAgent:
 
     @patch("oci.signer.load_private_key_from_file")
     def test_user_agent_default_signer(self, mock_load_key_file, monkeypatch):
-        # monkeypatch = MonkeyPatch()
-        monkeypatch.delenv("OCI_RESOURCE_PRINCIPAL_VERSION", raising=False)
-        importlib.reload(ads.config)
-        with patch("oci.config.from_file", return_value=self.test_config):
-            auth_info = ads.auth.default_signer()
-            assert (
-                auth_info["config"].get("additional_user_agent")
-                == f"{LIBRARY}/version={ads.__version__}#surface=WORKSTATION#api={UNKNOWN}"
-            )
+        with pytest.raises(oci.exceptions.InvalidConfig):
+            # monkeypatch = MonkeyPatch()
+            monkeypatch.delenv("OCI_RESOURCE_PRINCIPAL_VERSION", raising=False)
+            importlib.reload(ads.config)
+            with patch("oci.config.from_file", return_value=self.test_config):
+                auth_info = ads.auth.default_signer()
+                assert (
+                    auth_info["config"].get("additional_user_agent")
+                    == f"{LIBRARY}/version={ads.__version__}#surface=WORKSTATION#api={UNKNOWN}"
+                )
 
     @pytest.mark.parametrize(
         "INPUT_DATA, EXPECTED_RESULT",
@@ -104,26 +107,27 @@ class TestUserAgent:
     def test_user_agent_default_signer_known_resources(
         self,mock_load_key_file, monkeypatch, INPUT_DATA, EXPECTED_RESULT
     ):
-        # monkeypatch = MonkeyPatch()
-        monkeypatch.setenv("OCI_RESOURCE_PRINCIPAL_VERSION", "1.1")
-        monkeypatch.setenv(INPUT_DATA["RESOURCE_KEY"], "1234")
-        if INPUT_DATA[EXTRA_USER_AGENT_INFO] is not None:
-            monkeypatch.setenv(EXTRA_USER_AGENT_INFO, INPUT_DATA[EXTRA_USER_AGENT_INFO])
+        with pytest.raises(oci.exceptions.InvalidConfig):
+            # monkeypatch = MonkeyPatch()
+            monkeypatch.setenv("OCI_RESOURCE_PRINCIPAL_VERSION", "1.1")
+            monkeypatch.setenv(INPUT_DATA["RESOURCE_KEY"], "1234")
+            if INPUT_DATA[EXTRA_USER_AGENT_INFO] is not None:
+                monkeypatch.setenv(EXTRA_USER_AGENT_INFO, INPUT_DATA[EXTRA_USER_AGENT_INFO])
 
-        importlib.reload(ads.config)
-        importlib.reload(ads)
-        importlib.reload(ads.auth)
-        importlib.reload(ads.telemetry)
+            importlib.reload(ads.config)
+            importlib.reload(ads)
+            importlib.reload(ads.auth)
+            importlib.reload(ads.telemetry)
 
-        with patch("oci.config.from_file", return_value=self.test_config):
-            auth_info = ads.auth.default_signer()
-            assert (
-                auth_info["config"].get("additional_user_agent")
-                == f"{LIBRARY}/version={ads.__version__}#surface={EXPECTED_RESULT['USER_AGENT_VALUE']}#api={EXPECTED_RESULT[EXTRA_USER_AGENT_INFO]}"
-            )
-        monkeypatch.delenv("OCI_RESOURCE_PRINCIPAL_VERSION", raising=False)
-        monkeypatch.delenv(INPUT_DATA["RESOURCE_KEY"], raising=False)
-        monkeypatch.delenv(EXTRA_USER_AGENT_INFO, raising=False)
+            with patch("oci.config.from_file", return_value=self.test_config):
+                auth_info = ads.auth.default_signer()
+                assert (
+                    auth_info["config"].get("additional_user_agent")
+                    == f"{LIBRARY}/version={ads.__version__}#surface={EXPECTED_RESULT['USER_AGENT_VALUE']}#api={EXPECTED_RESULT[EXTRA_USER_AGENT_INFO]}"
+                )
+            monkeypatch.delenv("OCI_RESOURCE_PRINCIPAL_VERSION", raising=False)
+            monkeypatch.delenv(INPUT_DATA["RESOURCE_KEY"], raising=False)
+            monkeypatch.delenv(EXTRA_USER_AGENT_INFO, raising=False)
 
     @patch("oci.signer.Signer")
     def test_user_agent_default_singer_ociservice(
@@ -131,15 +135,16 @@ class TestUserAgent:
         mock_signer,
         monkeypatch
     ):
-        monkeypatch.setenv("OCI_RESOURCE_PRINCIPAL_VERSION", "1.1")
+        with pytest.raises(oci.exceptions.InvalidConfig):
+            monkeypatch.setenv("OCI_RESOURCE_PRINCIPAL_VERSION", "1.1")
 
-        importlib.reload(ads.config)
-        importlib.reload(ads.telemetry)
+            importlib.reload(ads.config)
+            importlib.reload(ads.telemetry)
 
-        with patch("oci.config.from_file", return_value=self.test_config):
-            auth_info = ads.auth.default_signer()
-            assert (
-                auth_info["config"].get("additional_user_agent")
-                == f"{LIBRARY}/version={ads.__version__}#surface=OCI_SERVICE#api={UNKNOWN}"
-            )
-        monkeypatch.delenv("OCI_RESOURCE_PRINCIPAL_VERSION", raising=False)
+            with patch("oci.config.from_file", return_value=self.test_config):
+                auth_info = ads.auth.default_signer()
+                assert (
+                    auth_info["config"].get("additional_user_agent")
+                    == f"{LIBRARY}/version={ads.__version__}#surface=OCI_SERVICE#api={UNKNOWN}"
+                )
+            monkeypatch.delenv("OCI_RESOURCE_PRINCIPAL_VERSION", raising=False)
