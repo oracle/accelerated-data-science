c26604a20e7dd7ea25797e29fe9c7fa39fc4f18c
lrudenka <liuda.rudenka@oracle.com>
2023-06-02 16:14:37 -0500

    ODSC-37151. Move ADS integration tests to the GitHub repository
    - remove debug=True in tests files

--- tests/integration/other/model_deployment_test_files/container_all/api.py
+++ tests/integration/other/model_deployment_test_files/container_all/api.py
@@ -65,4 +65,4 @@ def prediction():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
--- tests/integration/other/model_deployment_test_files/container_default/api.py
+++ tests/integration/other/model_deployment_test_files/container_default/api.py
@@ -65,4 +65,4 @@ def prediction():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8080)
