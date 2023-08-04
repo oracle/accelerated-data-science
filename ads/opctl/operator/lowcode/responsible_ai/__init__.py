7bb85bb2b1ce70fecdb1c3d3392e2bd35cb92777
Qiu Qin <qiu.qin@oracle.com>
2023-08-04 15:43:34 -0400

    Add responsible AI operator template.

---
+++ ads/opctl/operator/lowcode/responsible_ai/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*--
+
+# Copyright (c) 2023 Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
+
+__version__ = "v1"
+
+# __conda__ = "??" # switch to slug when service conda env will be available
+
+__conda__ = ""
+
+__keywords__ = []
+
+__short_description__ = """
+Responsible AI operator
+"""
+
+__description__ = """
+Magic begins here :)
+"""
---
+++ ads/opctl/operator/lowcode/responsible_ai/__main__.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*--
+
+# Copyright (c) 2023 Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
+
+import json
+import os
+import sys
+from typing import List
+
+import yaml
+
+from ads.opctl import logger
+from ads.opctl.operator.common.const import ENV_OPERATOR_ARGS
+from ads.opctl.operator.common.utils import _parse_input_args
+
+from .__init__ import __name__ as MODULE
+
+
+def main(raw_args: List[str]):
+    """The entry point of the forecasting the operator."""
+    args, _ = _parse_input_args(raw_args)
+    if not args.file and not args.spec and not os.environ.get(ENV_OPERATOR_ARGS):
+        logger.info(
+            "Please specify -f[--file] or -s[--spec] or "
+            f"pass operator's arguments via {ENV_OPERATOR_ARGS} environment variable."
+        )
+        return
+
+    logger.info("-" * 100)
+    logger.info(f"{'Running' if not args.verify else 'Verifying'} operator: {MODULE}")
+
+    # if spec provided as input string, then convert the string into YAML
+    operator_spec_str = args.spec or os.environ.get(ENV_OPERATOR_ARGS)
+
+    operator_config = yaml.safe_load(operator_spec_str)
+
+    # run operator
+    if args.verify:
+        # TODO: Add verify operator logic here
+        print(operator_config)
+    else:
+        # TODO: Add run operator logic here
+        print(operator_config)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
