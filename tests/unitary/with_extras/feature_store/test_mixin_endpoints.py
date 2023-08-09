55f2ee5f048e4faa22f641f0fb3f8bd7801af5a7
hvrai <harsh.rai@oracle.com>
2023-08-09 16:16:48 +0530

    ODSC-44564: Addressing unit tests review comments

--- tests/unitary/with_extras/feature_store/test_mixin_endpoints.py
+++ tests/unitary/with_extras/feature_store/test_mixin_endpoints.py
@@ -8,28 +8,53 @@ import ads
 from ads.feature_store.mixin.oci_feature_store import OCIFeatureStoreMixin
 from ads.common.auth import AuthType
 
-TEST_URL = "https://test.com"
+TEST_URL_1 = "https://test1.com"
+TEST_URL_2 = "https://test2.com"
+TEST_URL_3 = "https://test3.com"
+TEST_URL_4 = "https://test4.com"
 
 
-def test_manual_endpoint():
-    ads.set_auth(auth=AuthType.API_KEY, client_kwargs={"fs_service_endpoint": TEST_URL})
-    client = OCIFeatureStoreMixin.init_client(fs_service_endpoint=TEST_URL)
-    assert client.base_client.endpoint == f"{TEST_URL}/20230101"
+def test_global_service_endpoint():
+    ads.set_auth(auth=AuthType.API_KEY, client_kwargs={"service_endpoint": TEST_URL_1})
+    client = OCIFeatureStoreMixin.init_client()
+    assert client.base_client.endpoint == f"{TEST_URL_1}/20230101"
 
 
-def test_manual_with_service_endpoint():
+def test_global_service_and_fs_endpoints():
     ads.set_auth(
         auth=AuthType.API_KEY,
         client_kwargs={
-            "fs_service_endpoint": TEST_URL,
-            "service_endpoint": "service.com",
+            "fs_service_endpoint": TEST_URL_1,
+            "service_endpoint": TEST_URL_2,
         },
     )
-    client = OCIFeatureStoreMixin.init_client(fs_service_endpoint=TEST_URL)
-    assert client.base_client.endpoint == f"{TEST_URL}/20230101"
+    client = OCIFeatureStoreMixin.init_client()
+    assert client.base_client.endpoint == f"{TEST_URL_1}/20230101"
 
 
-def test_service_endpoint():
-    ads.set_auth(auth=AuthType.API_KEY, client_kwargs={"service_endpoint": TEST_URL})
-    client = OCIFeatureStoreMixin.init_client(fs_service_endpoint=TEST_URL)
-    assert client.base_client.endpoint == f"{TEST_URL}/20230101"
+def test_override_service_endpoint():
+    ads.set_auth(auth=AuthType.API_KEY)
+    client = OCIFeatureStoreMixin.init_client(service_endpoint=TEST_URL_1)
+    assert client.base_client.endpoint == f"{TEST_URL_1}/20230101"
+
+
+def test_override_service_and_fs_endpoints():
+    ads.set_auth(auth=AuthType.API_KEY)
+    client = OCIFeatureStoreMixin.init_client(
+        service_endpoint=TEST_URL_1, fs_service_endpoint=TEST_URL_2
+    )
+    assert client.base_client.endpoint == f"{TEST_URL_2}/20230101"
+
+
+def test_override_service_and_fs_endpoints_with_global_service_and_fs_endpoints():
+    ads.set_auth(
+        auth=AuthType.API_KEY,
+        client_kwargs={
+            "fs_service_endpoint": TEST_URL_3,
+            "service_endpoint": TEST_URL_4,
+        },
+    )
+    client = OCIFeatureStoreMixin.init_client(
+        service_endpoint=TEST_URL_1, fs_service_endpoint=TEST_URL_2
+    )
+    assert client.base_client.endpoint == f"{TEST_URL_2}/20230101"
