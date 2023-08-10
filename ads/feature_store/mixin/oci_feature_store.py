fb5075502ca8204ba9ab6c74b2bbf8918c301447
Divyank Gupta <divyank.gupta@oracle.com>
2023-08-10 15:22:18 +0530

    get_last_job interface

--- ads/feature_store/mixin/oci_feature_store.py
+++ ads/feature_store/mixin/oci_feature_store.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8; -*-
+from ads.common.decorator.utils import class_or_instance_method
 
 # Copyright (c) 2023 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
@@ -77,3 +78,44 @@ class OCIFeatureStoreMixin(OCIModelMixin):
     @property
     def client(self) -> oci.feature_store.feature_store_client.FeatureStoreClient:
         return super().client
+
+    @class_or_instance_method
+    def list_resource(
+        cls, compartment_id: str = None, limit: int = 0, **kwargs
+    ) -> list:
+        """Generic method to list OCI resources
+
+        Parameters
+        ----------
+        compartment_id : str
+            Compartment ID of the OCI resources. Defaults to None.
+            If compartment_id is not specified,
+            the value of NB_SESSION_COMPARTMENT_OCID in environment variable will be used.
+        limit : int
+            The maximum number of items to return. Defaults to 0, All items will be returned
+        **kwargs :
+            Additional keyword arguments to filter the resource.
+            The kwargs are passed into OCI API.
+
+        Returns
+        -------
+        list
+            A list of OCI resources
+
+        Raises
+        ------
+        NotImplementedError
+            List method is not supported or implemented.
+
+        """
+        if limit:
+            items = cls._find_oci_method("list")(
+                cls.check_compartment_id(compartment_id), limit=limit, **kwargs
+            ).data.items
+        else:
+            items = oci.pagination.list_call_get_all_results(
+                cls._find_oci_method("list"),
+                cls.check_compartment_id(compartment_id),
+                **kwargs,
+            ).data
+        return [cls.from_oci_model(item) for item in items]
