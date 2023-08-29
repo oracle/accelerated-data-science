45a53fc4eaa9c6a21b30916ec23cfb017bafaae2
lrudenka <liuda.rudenka@oracle.com>
2023-08-29 10:27:35 -0500

    ODSC-44598. Removed setup.py, added pyproject.toml instead
    - added setup.py back with comments to not use setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; -*-
+
+# Copyright (c) 2020, 2023 Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
+
+### File setup.py obsolete and must not be used. Please update pyproject.toml instead.
+### See detailed explanation why here:
+### https://blog.ganssle.io/articles/2021/10/setup-py-deprecated.html.
+# PEP 621 – Storing project metadata in pyproject.toml - https://peps.python.org/pep-0621/
+# PEP 518 – Specifying Minimum Build System Requirements for Python Projects https://peps.python.org/pep-0518/
+# PEP 508 – Dependency specification for Python Software Packages - https://peps.python.org/pep-0508/
+# PEP 517 – A build-system independent format for source trees - https://peps.python.org/pep-0517/
