9f829a392e47143bc8ac3e23ad0de9d9559c0635
Vikas Pandey <vikas.v.pandey@oracle.com>
2023-09-11 10:26:03 +0530

    add the default_trials to constants:

--- ads/opctl/operator/lowcode/forecast/model/neuralprophet.py
+++ ads/opctl/operator/lowcode/forecast/model/neuralprophet.py
@@ -25,6 +25,7 @@ from ads.common.decorator.runtime_dependency import (
 )
 from ads.opctl import logger
 
+from ...forecast.const import DEFAULT_TRIALS
 from .. import utils
 from .base_model import ForecastOperatorBaseModel
 
@@ -167,7 +168,7 @@ class NeuralProphetOperatorModel(ForecastOperatorBaseModel):
                 )
                 study.optimize(
                     objective,
-                    n_trials=self.spec.tuning.n_trials if self.spec.tuning else 10,
+                    n_trials=self.spec.tuning.n_trials if self.spec.tuning else DEFAULT_TRIALS,
                     n_jobs=-1,
                 )
 
