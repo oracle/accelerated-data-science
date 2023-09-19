0094970dcde7bfb29daf9b004d92079d15573ce1
hvrai <harsh.rai@oracle.com>
2023-09-19 14:22:34 +0530

    Adding box plot. Feature filter and refactoring files

---
+++ ads/feature_store/statistics/charts/frequency_distribution.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; -*-
+# Copyright (c) 2023 Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
+
+from typing import List
+from ads.common.decorator.runtime_dependency import OptionalDependency
+from ads.feature_store.statistics.charts.abstract_feature_stat import AbsFeatureStat
+
+try:
+    from plotly.graph_objs import Figure
+except ModuleNotFoundError:
+    raise ModuleNotFoundError(
+        f"The `plotly` module was not found. Please run `pip install "
+        f"{OptionalDependency.FEATURE_STORE}`."
+    )
+
+
+class FrequencyDistribution(AbsFeatureStat):
+    CONST_FREQUENCY = "frequency"
+    CONST_BINS = "bins"
+    CONST_FREQUENCY_DISTRIBUTION_TITLE = "Frequency Distribution"
+
+    def __init__(self, frequency: List, bins: List):
+        self.frequency = frequency
+        self.bins = bins
+
+    @classmethod
+    def from_json(cls, json_dict: dict) -> "FrequencyDistribution":
+        if json_dict is not None:
+            return FrequencyDistribution(
+                frequency=json_dict.get(cls.CONST_FREQUENCY),
+                bins=json_dict.get(cls.CONST_BINS),
+            )
+        else:
+            return None
+
+    def add_to_figure(self, fig: Figure, xaxis: int, yaxis: int):
+        xaxis_str, yaxis_str, x_str, y_str = self.get_x_y_str_axes(xaxis, yaxis)
+        if (
+            type(self.frequency) == list
+            and type(self.bins) == list
+            and 0 < len(self.frequency) == len(self.bins) > 0
+        ):
+            fig.add_bar(
+                x=self.bins, y=self.frequency, xaxis=x_str, yaxis=y_str, name=""
+            )
+            fig.layout.annotations[xaxis].text = self.CONST_FREQUENCY_DISTRIBUTION_TITLE
+            fig.layout[xaxis_str]["title"] = "Bins"
+            fig.layout[yaxis_str]["title"] = "Frequency"
