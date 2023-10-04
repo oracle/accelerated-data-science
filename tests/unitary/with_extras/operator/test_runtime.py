00815f1d6ad2c230d278f7d3eea450c2443c621d
Dmitrii Cherkasov <dmitrii.cherkasov@oracle.com>
2023-10-03 22:12:36 -0700

    Adds tests for the operator yaml generator.

--- tests/unitary/with_extras/operator/test_runtime.py
+++ tests/unitary/with_extras/operator/test_runtime.py
@@ -4,14 +4,52 @@
 # Copyright (c) 2023 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 
+import pytest
+from my_module.runtime import ContainerRuntime, PythonRuntime
+
 
 class TestContainerRuntime:
-    """Tests operator container local runtime."""
+    def test_init(self):
+        runtime = ContainerRuntime(image="my-image", command="my-command")
+        self.assertEqual(runtime.image, "my-image")
+        self.assertEqual(runtime.command, "my-command")
 
-    pass
+    def test_to_dict(self):
+        runtime = ContainerRuntime(image="my-image", command="my-command")
+        runtime_dict = runtime.to_dict()
+        self.assertIsInstance(runtime_dict, dict)
+        self.assertEqual(runtime_dict["type"], "container")
+        self.assertEqual(runtime_dict["image"], "my-image")
+        self.assertEqual(runtime_dict["command"], "my-command")
+
+    def test_from_dict(self):
+        runtime_dict = {
+            "type": "container",
+            "image": "my-image",
+            "command": "my-command",
+        }
+        runtime = ContainerRuntime.from_dict(runtime_dict)
+        self.assertIsInstance(runtime, ContainerRuntime)
+        self.assertEqual(runtime.image, "my-image")
+        self.assertEqual(runtime.command, "my-command")
 
 
 class TestPythonRuntime:
-    """Tests operator python local runtime."""
+    def test_init(self):
+        runtime = PythonRuntime(version="v2")
+        self.assertEqual(runtime.version, "v2")
+        self.assertEqual(runtime.type, "python")
 
-    pass
+    def test_to_dict(self):
+        runtime = PythonRuntime(version="v2")
+        runtime_dict = runtime.to_dict()
+        self.assertIsInstance(runtime_dict, dict)
+        self.assertEqual(runtime_dict["type"], "python")
+        self.assertEqual(runtime_dict["version"], "v2")
+
+    def test_from_dict(self):
+        runtime_dict = {"type": "python", "version": "v2"}
+        runtime = PythonRuntime.from_dict(runtime_dict)
+        self.assertIsInstance(runtime, PythonRuntime)
+        self.assertEqual(runtime.version, "v2")
+        self.assertEqual(runtime.type, "python")
