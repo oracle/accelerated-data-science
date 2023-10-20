bab2e389d81dea56e3543c11671144377ac99c0f
najiyacl <fathimath.najiya.c.chemnad.le@oracle.com>
2023-10-20 18:52:00 +0530

    dataset changes

--- ads/feature_store/dataset.py
+++ ads/feature_store/dataset.py
@@ -1101,7 +1101,12 @@ class Dataset(Builder):
         feature_statistics = (
             output_details.get("featureStatistics") if output_details else None
         )
-        return Statistics(feature_statistics)
+        stat_version = (
+            output_details.get("version") if output_details else None
+        )
+        version = stat_version if stat_version is not None else 1
+
+        return Statistics(feature_statistics, version)
 
     def get_validation_output(self, job_id: str = None) -> "ValidationOutput":
         """Retrieve Statistics object for the job with job_id
