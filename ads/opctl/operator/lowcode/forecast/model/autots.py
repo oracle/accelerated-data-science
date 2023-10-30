43e9d0470028846965197df4efe3bf5a75c6525f
govarsha <goalla.v.varsha@oracle.com>
2023-10-30 22:12:58 +0530

    changes to enable the model_kwargs dict to be passed directly through to AutoTS.

--- ads/opctl/operator/lowcode/forecast/model/autots.py
+++ ads/opctl/operator/lowcode/forecast/model/autots.py
@@ -46,24 +46,26 @@ class AutoTSOperatorModel(ForecastOperatorBaseModel):
         # Initialize the AutoTS model with specified parameters
         model = AutoTS(
             forecast_length=self.spec.horizon.periods,
-            frequency="infer",
+            frequency=self.spec.model_kwargs.get("frequency", "infer"),
             prediction_interval=self.spec.confidence_interval_width,
             max_generations=self.spec.model_kwargs.get(
                 "max_generations", AUTOTS_MAX_GENERATION
             ),
-            no_negatives=False,
-            constraint=None,
+            no_negatives=self.spec.model_kwargs.get("no_negatives", False),
+            constraint=self.spec.model_kwargs.get("constraint", None),
             ensemble=self.spec.model_kwargs.get("ensemble", "auto"),
             initial_template=self.spec.model_kwargs.get(
                 "initial_template", "General+Random"
             ),
-            random_seed=2022,
+            random_seed=self.spec.model_kwargs.get("random_seed", 2022),
             holiday_country=self.spec.model_kwargs.get("holiday_country", "US"),
-            subset=None,
-            aggfunc="first",
-            na_tolerance=1,
-            drop_most_recent=0,
-            drop_data_older_than_periods=None,
+            subset=self.spec.model_kwargs.get("subset", None),
+            aggfunc=self.spec.model_kwargs.get("aggfunc", "first"),
+            na_tolerance=self.spec.model_kwargs.get("na_tolerance", 1),
+            drop_most_recent=self.spec.model_kwargs.get("drop_most_recent", 0),
+            drop_data_older_than_periods=self.spec.model_kwargs.get(
+                "drop_data_older_than_periods", None
+            ),
             model_list=self.spec.model_kwargs.get("model_list", "fast_parallel"),
             transformer_list=self.spec.model_kwargs.get("transformer_list", "auto"),
             transformer_max_depth=self.spec.model_kwargs.get(
@@ -74,22 +76,24 @@ class AutoTSOperatorModel(ForecastOperatorBaseModel):
             models_to_validate=self.spec.model_kwargs.get(
                 "models_to_validate", AUTOTS_MODELS_TO_VALIDATE
             ),
-            max_per_model_class=None,
+            max_per_model_class=self.spec.model_kwargs.get("max_per_model_class", None),
             validation_method=self.spec.model_kwargs.get(
                 "validation_method", "backwards"
             ),
             min_allowed_train_percent=self.spec.model_kwargs.get(
                 "min_allowed_train_percent", 0.5
             ),
-            remove_leading_zeroes=False,
-            prefill_na=None,
-            introduce_na=None,
-            preclean=None,
-            model_interrupt=True,
-            generation_timeout=None,
-            current_model_file=None,
-            verbose=1,
-            n_jobs=-1,
+            remove_leading_zeroes=self.spec.model_kwargs.get(
+                "remove_leading_zeros", False
+            ),
+            prefill_na=self.spec.model_kwargs.get("prefill_na", None),
+            introduce_na=self.spec.model_kwargs.get("introduce_na", None),
+            preclean=self.spec.model_kwargs.get("preclean", None),
+            model_interrupt=self.spec.model_kwargs.get("model_interrupt", True),
+            generation_timeout=self.spec.model_kwargs.get("generation_timeout", None),
+            current_model_file=self.spec.model_kwargs.get("current_model_life", None),
+            verbose=self.spec.model_kwargs.get("verbose", 1),
+            n_jobs=self.spec.model_kwargs.get("n_jobs", -1),
         )
 
         # Prepare the data for model training
@@ -226,7 +230,9 @@ class AutoTSOperatorModel(ForecastOperatorBaseModel):
             ),
             target_columns=self.target_columns,
         )
-
+        print("-------------------------------------------------")
+        print(self.models)
+        print("-------------------------------------------------")
         # Section 2: AutoTS Model Parameters
         sec2_text = dp.Text(f"## AutoTS Model Parameters")
         # TODO: ODSC-47612 Format the parameters better for display in report.
