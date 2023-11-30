3418eea5c9d87e42895b8c861292d7b7a163e182
Qiu Qin <qiu.qin@oracle.com>
2023-11-30 11:36:18 -0500

    Update compartment_id validation for GenAI models.

--- ads/llm/langchain/plugins/base.py
+++ ads/llm/langchain/plugins/base.py
@@ -56,10 +56,12 @@ class BaseLLM(LLM, Serializable):
 
 
 class GenerativeAiClientModel(BaseModel):
+    """Base model for generative AI embedding model and LLM."""
+
     client: Any  #: :meta private:
     """OCI GenerativeAiClient."""
 
-    compartment_id: str
+    compartment_id: str = None
     """Compartment ID of the caller."""
 
     endpoint_kwargs: Dict[str, Any] = {}
@@ -90,7 +92,9 @@ class GenerativeAiClientModel(BaseModel):
             client_kwargs.update(values["client_kwargs"])
             values["client"] = GenerativeAiClient(**auth, **client_kwargs)
         # Set default compartment ID
-        if "compartment_id" not in values and COMPARTMENT_OCID:
-            values["compartment_id"] = COMPARTMENT_OCID
-
+        if not values.get("compartment_id"):
+            if COMPARTMENT_OCID:
+                values["compartment_id"] = COMPARTMENT_OCID
+            else:
+                raise ValueError("Please specify compartment_id.")
         return values
