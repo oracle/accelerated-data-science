e38021972a159ee63b6897d17a2b2e7dbc96d516
Allen <allen.hosler@oracle.com>
2023-12-14 13:01:36 +0000

    get rid of temp folder

--- ads/opctl/operator/common/utils.py
+++ ads/opctl/operator/common/utils.py
@@ -26,6 +26,45 @@ class OperatorValidator(Validator):
     pass
 
 
+def path_exists(uri: str, auth: Optional[Dict] = None) -> bool:
+    """Check if the given path exists. It can be a local path or an OCI object storage URI.
+
+    Parameters
+    ----------
+    uri: str
+        The URI of the target, which can be local path or OCI object storage URI.
+    auth: (Dict, optional). Defaults to None.
+        The default authentication is set using `ads.set_auth` API. If you need to override the
+        default, use the `ads.common.auth.api_keys` or `ads.common.auth.resource_principal` to create appropriate
+        authentication signer and kwargs required to instantiate IdentityClient object.
+
+    Returns
+    -------
+    bool: return True if the path exists.
+    """
+    protocol = fsspec.utils.get_protocol(uri)
+    storage_options = {}
+    if protocol != "file":
+        storage_options = auth or authutil.default_signer()
+    return fsspec.filesystem(protocol, **storage_options).exists(uri)
+
+
+def create_output_folder(name):
+    output_folder = name
+    protocol = fsspec.utils.get_protocol(output_folder)
+    storage_options = {}
+    if protocol != "file":
+        storage_options = auth or authutil.default_signer()  # TODO update auth
+
+    fs = fsspec.filesystem(protocol, **storage_options)
+    name_suffix = 1
+    while fs.exists(output_folder):
+        name_suffix = name_suffix + 1
+        output_folder = f"{name}_{name_suffix}"
+    fs.mkdirs(output_folder)
+    return output_folder
+
+
 def _build_image(
     dockerfile: str,
     image_name: str,
