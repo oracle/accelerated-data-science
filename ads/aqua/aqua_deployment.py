f8601a36b371a9e5c2263af8b3a360cba63198f4
Vipul <vipul.mascarenhas@oracle.com>
2024-01-03 14:56:19 -0800

    created empty aqua deployment wrapper

---
+++ ads/aqua/aqua_deployment.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*--
+# Copyright (c) 2024 Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
+
+import logging
+from typing import List, Dict
+from ads.model.deployment import ModelDeployment
+
+logger = logging.getLogger(__name__)
+
+
+class AquaDeployment(ModelDeployment):
+    """
+
+    Attributes
+    ----------
+
+    Methods
+    -------
+    create(self, **kwargs) -> "AquaDeployment"
+        Creates an instance of Aqua deployment.
+    list(self, ..., **kwargs) -> List["AquaDeployment"]
+        List existing model deployments created via Aqua
+    clone()
+        Clone an existing model deployment
+    suggest()
+        Provide suggestions for model deployment
+    stats()
+        Get model deployment statistics
+    """
+
+    def __init__(self, **kwargs):
+        """Initializes an Aqua model deployment."""
+        super().__init__(**kwargs)
+
+    def create(self, **kwargs) -> "AquaDeployment":
+        pass
+
+    def list(self, **kwargs) -> List["AquaDeployment"]:
+        pass
+
+    def clone(self, **kwargs) -> "AquaDeployment":
+        pass
+
+    def suggest(self, **kwargs) -> Dict:
+        pass
+
+    def stats(self, **kwargs) -> Dict:
+        pass
