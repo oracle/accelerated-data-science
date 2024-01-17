95793fa00899d75eb6b81801e14002842f30df88
Qiu Qin <qiu.qin@oracle.com>
2024-01-17 10:55:03 -0500

    Add Aqua extension examples.

---
+++ ads/aqua/extension/base_handler.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2024 Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
+
+
+import json
+from dataclasses import asdict, is_dataclass
+from typing import Any
+from notebook.base.handlers import APIHandler
+
+
+class AquaAPIhandler(APIHandler):
+    """Base handler for Aqua REST APIs."""
+
+    @staticmethod
+    def serialize(obj: Any):
+        """Serialize the object.
+        If the object is a dataclass, convert it to dictionary.
+        Otherwise, convert it to string.
+        """
+        if is_dataclass(obj):
+            return asdict(obj)
+        return str(obj)
+
+    def finish(self, payload, status_code: int = 200):
+        """Ending the HTTP request by returning a payload and status code."""
+        # Set status code
+        self.set_status(status_code)
+        # If the payload is a list, put into a dictionary with key=data
+        if isinstance(payload, list):
+            payload = {"data": payload}
+        # Convert the payload to a JSON serializable object
+        payload = json.loads(json.dumps(payload, default=self.serialize))
+        return super().finish(payload)
