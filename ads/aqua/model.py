44a3fce1d2d636055a8584d0148fa10640f30d5d
Lu Peng <bolu.peng@oracle.com>
2024-01-23 18:35:37 -0500

    Updated pr.

--- ads/aqua/model.py
+++ ads/aqua/model.py
@@ -57,7 +57,35 @@ class AquaModelApp(AquaApp):
         pass
 
     def get(self, model_id) -> "AquaModel":
-        """Gets the information of an Aqua model."""        
+        """Gets the information of an Aqua model."""
+        model_card = """
+# Model Card: Dummy Text Generator
+## Description
+This is a simple dummy text generator model developed using Hugging Face's Transformers library. It generates random text based on a pre-trained language model.
+## Model Details
+- Model Name: DummyTextGenerator
+- Model Architecture: GPT-2
+- Model Size: 125M parameters
+- Training Data: Random text from the internet
+## Usage
+You can use this model to generate dummy text for various purposes, such as testing text processing pipelines or generating placeholder text for design mockups.
+Here's an example of how to use it in Python:
+```python
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+model_name = "dummy-text-generator"
+tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+model = GPT2LMHeadModel.from_pretrained(model_name)
+prompt = "Once upon a time"
+input_ids = tokenizer.encode(prompt, return_tensors="pt")
+output = model.generate(input_ids, max_length=50, num_return_sequences=1, pad_token_id=tokenizer.eos_token_id)
+generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
+print(generated_text)
+```
+## Evaluation
+The model does not perform any meaningful text generation but can be used for basic testing purposes.
+## License
+This model is released under the MIT License.
+"""
         return AquaModel(
             **{
                 "compartment_id": "ocid1.compartment.oc1..xxxx",
@@ -72,7 +100,7 @@ class AquaModelApp(AquaApp):
                 "license": "Apache 2.0",
                 "organization": "Meta AI",
                 "is_fine_tuned": False,
-                "model_card": "This will be the content of the model card from Huggingface."
+                "model_card": model_card
             }
         )
 
