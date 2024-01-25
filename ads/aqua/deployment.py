ce073393de57151078fe1ca9667e2ba4ac41d2a3
Vipul <vipul.mascarenhas@oracle.com>
2024-01-24 17:54:47 -0800

    create deployment

--- ads/aqua/deployment.py
+++ ads/aqua/deployment.py
@@ -8,16 +8,22 @@ from typing import List, Dict
 
 from dataclasses import dataclass
 from ads.aqua.base import AquaApp
-
+from ads.model.deployment import (
+    ModelDeployment,
+    ModelDeploymentInfrastructure,
+    ModelDeploymentContainerRuntime,
+)
 
 logger = logging.getLogger(__name__)
+
 
 @dataclass
 class AquaDeployment:
     """Represents an Aqua Model Deployment"""
+
     display_name: str
     aqua_service_model: str
-    state: str
+    status: str
     description: str
     created_on: str
     created_by: str
@@ -43,8 +49,80 @@ class AquaDeploymentApp(AquaApp):
         Get model deployment statistics
     """
 
-    def create(self, **kwargs) -> "AquaDeployment":
-        pass
+    def create(
+        self,
+        compartment_id: str = None,
+        project_id: str = None,
+        region: str = None,
+        model_id: str = None,
+        display_name: str = None,
+        description: str = None,
+        instance_count: int = None,
+        instance_shape: str = None,
+        access_log_group_id: str = None,
+        access_log_id: str = None,
+        predict_log_group_id: str = None,
+        predict_log_id: str = None,
+        bandwidth: int = None,
+        web_concurrency: int = None,
+        deployment_image: str = None,
+        server_port: int = None,
+        health_check_port: int = None,
+        wait_for_completion: bool = False,
+        **kwargs,
+    ) -> "AquaDeployment":
+        env_var = {}
+        # configure model deployment infrastructure
+        infrastructure = (
+            ModelDeploymentInfrastructure()
+            .with_project_id(project_id)
+            .with_compartment_id(compartment_id)
+            .with_shape_name(instance_shape)
+            .with_bandwidth_mbps(bandwidth)
+            .with_replica(instance_count)
+            .with_web_concurrency(web_concurrency)
+            .with_access_log(
+                log_group_id=access_log_group_id,
+                log_id=access_log_id,
+            )
+            .with_predict_log(
+                log_group_id=predict_log_group_id,
+                log_id=predict_log_id,
+            )
+        )
+        # configure model deployment runtime
+        container_runtime = (
+            ModelDeploymentContainerRuntime()
+            .with_image(deployment_image)
+            .with_server_port(server_port)
+            .with_health_check_port(health_check_port)
+            .with_env(env_var)
+            .with_deployment_mode("HTTPS_ONLY")
+            .with_model_uri(model_id)
+            .with_region(region)
+            .with_overwrite_existing_artifact(False)
+            .with_remove_existing_artifact(False)
+        )
+        # configure model deployment
+        deployment = (
+            ModelDeployment()
+            .with_display_name(display_name)
+            .with_description(description)
+            .with_freeform_tags()
+            .with_infrastructure(infrastructure)
+            .with_runtime(container_runtime)
+        )
+        # Deploy model on container runtime
+        deployment.deploy(wait_for_completion=wait_for_completion)
+
+        return AquaDeployment(
+            display_name=deployment.display_name,
+            aqua_service_model="",
+            status=deployment.status.name,
+            description=deployment.description,
+            created_on=deployment.time_created,
+            created_by=deployment.created_by,
+        )
 
     def list(self, **kwargs) -> List["AquaDeployment"]:
         return [
@@ -52,19 +130,11 @@ class AquaDeploymentApp(AquaApp):
                 **{
                     "display_name": f"aqua model deployment {i}",
                     "aqua_service_model": f"aqua service model {i}",
-                    "state": "ACTIVE" if i%2==0 else "FAILED",
+                    "state": "ACTIVE" if i % 2 == 0 else "FAILED",
                     "description": "test description",
                     "created_on": "test created on",
-                    "created_by": "test created by"
+                    "created_by": "test created by",
                 }
-            ) for i in range(8)
+            )
+            for i in range(8)
         ]
-
-    def clone(self, **kwargs) -> "AquaDeployment":
-        pass
-
-    def suggest(self, **kwargs) -> Dict:
-        pass
-
-    def stats(self, **kwargs) -> Dict:
-        pass
--- ads/aqua/extension/deployment_handler.py
+++ ads/aqua/extension/deployment_handler.py
@@ -9,10 +9,28 @@ from ads.aqua.deployment import AquaDeploymentApp
 
 
 class AquaDeploymentHandler(AquaAPIhandler):
-    """Handler for Aqua Model REST APIs."""
+    """
+    Handler for Aqua Deployment REST APIs.
 
-    def get(self, model_id=""):
+    Methods
+    -------
+    get(self, id="")
+        Retrieves a list of AQUA deployments or model info or logs by ID.
+    post(self, *args, **kwargs)
+        Creates a new AQUA deployment.
+    read(self, id: str)
+        Reads the AQUA deployment information.
+    list(self)
+        Lists all the AQUA deployments.
+
+    Raises
+    ------
+    HTTPError: For various failure scenarios such as invalid input format, missing data, etc.
+    """
+
+    def get(self, id=""):
         """Handle GET request."""
+        # todo: handle list, read and logs for model deployment
         pass
 
     def post(self, **kwargs):
@@ -32,36 +50,39 @@ class AquaDeploymentHandler(AquaAPIhandler):
             raise HTTPError(400, Errors.NO_INPUT_DATA)
 
         try:
-            model_id = input_data.get("model_id")
+            # todo: identify optional params
             compartment_id = input_data.get("compartment_id")
             project_id = input_data.get("project_id", None)
+            model_id = input_data.get("model_id")
             display_name = input_data.get("display_name")
             description = input_data.get("description")
             instance_count = input_data.get("instance_count")
-            shape = input_data.get("shape")
+            instance_shape = input_data.get("instance_shape")
             access_log_group_id = input_data.get("access_log_group_id")
             access_log_id = input_data.get("access_log_id")
             predict_log_group_id = input_data.get("predict_log_group_id")
             predict_log_id = input_data.get("predict_log_id")
             bandwidth = input_data.get("bandwidth")
         except Exception as ex:
+            # todo: handle missing parameter error
             raise HTTPError(400, Errors.MISSING_REQUIRED_PARAMETER.format(ex))
 
         try:
             self.finish(
                 AquaDeploymentApp().create(
-                    model_id=model_id,
                     compartment_id=compartment_id,
                     project_id=project_id,
+                    model_id=model_id,
                     display_name=display_name,
                     description=description,
                     instance_count=instance_count,
-                    shape=shape,
+                    instance_shape=instance_shape,
                     access_log_group_id=access_log_group_id,
                     access_log_id=access_log_id,
                     predict_log_group_id=predict_log_group_id,
                     predict_log_id=predict_log_id,
                     bandwidth=bandwidth,
+                    wait_for_completion=False,
                 )
             )
         except Exception as ex:
