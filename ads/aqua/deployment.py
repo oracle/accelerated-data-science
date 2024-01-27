ce402dc2058d4603e0bc90e7094c03eb1d40be90
Vipul <vipul.mascarenhas@oracle.com>
2024-01-26 17:14:54 -0800

    updated handler

--- ads/aqua/deployment.py
+++ ads/aqua/deployment.py
@@ -56,74 +56,137 @@ class AquaDeploymentApp(AquaApp):
         project_id: str = None,
         region: str = None,
         model_id: str = None,
-        aqua_service_model_tag: str = None,
+        aqua_service_model: str = None,
         display_name: str = None,
         description: str = None,
         instance_count: int = None,
         instance_shape: str = None,
-        access_log_group_id: str = None,
+        log_group_id: str = None,
         access_log_id: str = None,
-        predict_log_group_id: str = None,
         predict_log_id: str = None,
-        bandwidth: int = None,
+        bandwidth_mbps: int = None,
         web_concurrency: int = None,
         deployment_image: str = None,
-        server_port: int = None,
-        health_check_port: int = None,
-        wait_for_completion: bool = False,
-        **kwargs,
+        entrypoint: List[str] = None,
+        server_port: int = 5000,
+        health_check_port: int = 5000,
+        env_var: Dict = None,
     ) -> "AquaDeployment":
-        env_var = {}
+        """
+        Creates a new Aqua deployment
+
+        Parameters
+        ----------
+        model_id: str
+            The model OCID to deploy.
+        compartment_id: str
+            The compartment OCID
+        project_id: str
+            Target project to list deployments from.
+        region: str (None)
+            The Region Identifier that the client should connect to.
+            Regions can be found here:
+            https://docs.oracle.com/en-us/iaas/Content/General/Concepts/regions.htm
+        aqua_service_model: str
+            The aqua tag that gets passed from the model that identifies the type of model
+        display_name: str
+            The name of model deployment.
+        description: str
+            The description of the deployment.
+        instance_count: (int, optional). Defaults to 1.
+            The number of instance used for deployment.
+        instance_shape: (str, optional). Default to `VM.Standard2.1`.
+            The shape of the instance used for deployment.
+        log_group_id: (str, optional)
+            The oci logging group id. The access log and predict log share the same log group.
+        access_log_id: (str, optional). Defaults to None.
+            The access log OCID for the access logs. https://docs.oracle.com/en-us/iaas/data-science/using/model_dep_using_logging.htm
+        predict_log_id: (str, optional). Defaults to None.
+            The predict log OCID for the predict logs. https://docs.oracle.com/en-us/iaas/data-science/using/model_dep_using_logging.htm
+        bandwidth_mbps: (int, optional). Defaults to 10.
+            The bandwidth limit on the load balancer in Mbps.
+        web_concurrency: str
+            The number of worker processes/threads to handle incoming requests
+        deployment_image: (str, optional). Defaults to None.
+            The OCIR path of docker container image. Required for deploying model on container runtime.
+        with_bucket_uri(bucket_uri)
+            Sets the bucket uri when uploading large size model.
+        entrypoint: (List, optional). Defaults to empty.
+            The entrypoint for running docker container image.
+        server_port: (int, optional). Defaults to 8080.
+            The server port for docker container image.
+        health_check_port: (int, optional). Defaults to 8080.
+            The health check port for docker container image.
+        env_var : dict, optional
+            Environment variable for the deployment, by default None.
+        Returns
+        -------
+        Session
+            The playground session instance.
+
+        Raises
+        ------
+        ValueError
+            If model ID not provided.
+        """
+        # todo: create a model catalog entry with model path pointing to service bucket
+
+        # Start model deployment
         # configure model deployment infrastructure
+        # todo : any other infrastructure params needed?
         infrastructure = (
             ModelDeploymentInfrastructure()
             .with_project_id(project_id)
             .with_compartment_id(compartment_id)
             .with_shape_name(instance_shape)
-            .with_bandwidth_mbps(bandwidth)
+            .with_bandwidth_mbps(bandwidth_mbps)
             .with_replica(instance_count)
             .with_web_concurrency(web_concurrency)
             .with_access_log(
-                log_group_id=access_log_group_id,
+                log_group_id=log_group_id,
                 log_id=access_log_id,
             )
             .with_predict_log(
-                log_group_id=predict_log_group_id,
+                log_group_id=log_group_id,
                 log_id=predict_log_id,
             )
         )
+        logger.info(f"Infra: {infrastructure}")
         # configure model deployment runtime
+        # todo : any other runtime params needed?
         container_runtime = (
             ModelDeploymentContainerRuntime()
             .with_image(deployment_image)
+            .with_entrypoint(entrypoint)
             .with_server_port(server_port)
             .with_health_check_port(health_check_port)
             .with_env(env_var)
+            .with_deployment_mode("HTTPS_ONLY")
             .with_model_uri(model_id)
             .with_region(region)
             .with_overwrite_existing_artifact(False)
             .with_remove_existing_artifact(False)
         )
-        # configure model deployment
+        logger.info(f"Infra: {container_runtime}")
+        # configure model deployment and deploy model on container runtime
+        # todo : any other deployment params needed?
         deployment = (
             ModelDeployment()
             .with_display_name(display_name)
             .with_description(description)
-            .with_freeform_tags()
+            .with_freeform_tags(aqua_service_model=aqua_service_model)
             .with_infrastructure(infrastructure)
             .with_runtime(container_runtime)
-        )
-        # Deploy model on container runtime
-        deployment.deploy(wait_for_completion=wait_for_completion)
+        ).deploy(wait_for_completion=False)
 
         return AquaDeployment(
             display_name=deployment.display_name,
-            aqua_service_model=aqua_service_model_tag,
+            aqua_service_model=aqua_service_model,
             status=deployment.status.name,
             description=deployment.description,
             created_on=deployment.time_created,
             created_by=deployment.created_by,
-            endpoint=deployment.model_deployment_url,
+            endpoint=deployment.url,
         )
 
     def list(self, **kwargs) -> List["AquaDeployment"]:
--- ads/aqua/extension/deployment_handler.py
+++ ads/aqua/extension/deployment_handler.py
@@ -6,6 +6,7 @@
 from tornado.web import HTTPError
 from ads.aqua.extension.base_handler import AquaAPIhandler, Errors
 from ads.aqua.deployment import AquaDeploymentApp
+from ads.aqua.model import AquaModelApp
 
 
 class AquaDeploymentHandler(AquaAPIhandler):
@@ -33,7 +34,7 @@ class AquaDeploymentHandler(AquaAPIhandler):
         # todo: handle list, read and logs for model deployment
         pass
 
-    def post(self, **kwargs):
+    def post(self, *args, **kwargs):
         """
         Handles post request for the deployment APIs
         Raises
@@ -49,40 +50,77 @@ class AquaDeploymentHandler(AquaAPIhandler):
         if not input_data:
             raise HTTPError(400, Errors.NO_INPUT_DATA)
 
-        try:
-            # todo: identify optional params
-            compartment_id = input_data.get("compartment_id")
-            project_id = input_data.get("project_id", None)
-            model_id = input_data.get("model_id")
-            display_name = input_data.get("display_name")
-            description = input_data.get("description")
-            instance_count = input_data.get("instance_count")
-            instance_shape = input_data.get("instance_shape")
-            access_log_group_id = input_data.get("access_log_group_id")
-            access_log_id = input_data.get("access_log_id")
-            predict_log_group_id = input_data.get("predict_log_group_id")
-            predict_log_id = input_data.get("predict_log_id")
-            bandwidth = input_data.get("bandwidth")
-        except Exception as ex:
-            # todo: handle missing parameter error
-            raise HTTPError(400, Errors.MISSING_REQUIRED_PARAMETER.format(ex))
+        # required input parameters
+        compartment_id = input_data.get("compartment_id")
+        if not compartment_id:
+            raise HTTPError(
+                400, Errors.MISSING_REQUIRED_PARAMETER.format("compartment_id")
+            )
+        display_name = input_data.get("display_name")
+        if not display_name:
+            raise HTTPError(
+                400, Errors.MISSING_REQUIRED_PARAMETER.format("display_name")
+            )
+        instance_count = input_data.get("instance_count")
+        if not instance_count:
+            raise HTTPError(
+                400, Errors.MISSING_REQUIRED_PARAMETER.format("instance_count")
+            )
+        instance_shape = input_data.get("instance_shape")
+        if not instance_count:
+            raise HTTPError(
+                400, Errors.MISSING_REQUIRED_PARAMETER.format("instance_shape")
+            )
+        log_group_id = input_data.get("log_group_id")
+        if not log_group_id:
+            raise HTTPError(
+                400, Errors.MISSING_REQUIRED_PARAMETER.format("log_group_id")
+            )
+        access_log_id = input_data.get("access_log_id")
+        if not access_log_id:
+            raise HTTPError(
+                400, Errors.MISSING_REQUIRED_PARAMETER.format("access_log_id")
+            )
+        predict_log_id = input_data.get("predict_log_id")
+        if not predict_log_id:
+            raise HTTPError(
+                400, Errors.MISSING_REQUIRED_PARAMETER.format("predict_log_id")
+            )
+
+        # todo: remove this & replace with id from aqua model
+        model_id = input_data.get("model_id")
+        if not instance_count:
+            raise HTTPError(400, Errors.MISSING_REQUIRED_PARAMETER.format("model_id"))
+
+        project_id = input_data.get("project_id")
+        description = input_data.get("description")
+        bandwidth_mbps = input_data.get("bandwidth_mbps", 10)
 
         try:
+            # todo: call create method to create a catalog entry
+            # aqua_model = AquaModelApp.create()
+            # get deployment_image from aqua model
+            deployment_image = "iad.ocir.io/ociodscdev/aqua_deploy:1.0.0"
+            entrypoint = ["python", "/opt/api/api.py"]
+            aqua_service_model = f"xxx.xxx.xxx.xxx.xxx#Mistral-7B-Instruct-v0.1"
+
             self.finish(
                 AquaDeploymentApp().create(
                     compartment_id=compartment_id,
                     project_id=project_id,
+                    # todo: replace model_id with aqua_model.id
                     model_id=model_id,
+                    aqua_service_model=aqua_service_model,
                     display_name=display_name,
                     description=description,
                     instance_count=instance_count,
                     instance_shape=instance_shape,
-                    access_log_group_id=access_log_group_id,
+                    log_group_id=log_group_id,
                     access_log_id=access_log_id,
-                    predict_log_group_id=predict_log_group_id,
                     predict_log_id=predict_log_id,
-                    bandwidth=bandwidth,
-                    wait_for_completion=False,
+                    bandwidth_mbps=bandwidth_mbps,
+                    deployment_image=deployment_image,
+                    entrypoint=entrypoint,
                 )
             )
         except Exception as ex:
