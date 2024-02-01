86b75c02be5dd3d9f269360412e3cf6795e63eb6
Vipul <vipul.mascarenhas@oracle.com>
2024-02-01 13:55:00 -0800

    updated client

--- ads/aqua/base.py
+++ ads/aqua/base.py
@@ -11,6 +11,8 @@ import oci
 from ads.common.auth import default_signer
 from ads.aqua.exception import AquaServiceError, AquaClientError
 from oci.exceptions import ServiceError, ClientError
+from ads.common import oci_client as oc
+from ads.common.utils import extract_region
 
 logger = logging.getLogger(__name__)
 
@@ -20,7 +22,10 @@ class AquaApp:
 
     def __init__(self) -> None:
         self._auth = default_signer()
-        self.client = oci.data_science.DataScienceClient(**self._auth)
+        self.ds_client = oc.OCIClientFactory(**self._auth).data_science
+        self.logging_client = oc.OCIClientFactory(**self._auth).logging_management
+        self.identity_client = oc.OCIClientFactory(**self._auth).identity
+        self.region = extract_region(self._auth)
 
     def list_resource(
         self,
