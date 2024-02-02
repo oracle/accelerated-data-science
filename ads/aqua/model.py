73f8f7c803d50055a812a92bc66b6d5044ae6dbc
MING KANG <ming.kang@oracle.com>
2024-02-01 20:21:55 -0500

    reverted _if_show

--- ads/aqua/model.py
+++ ads/aqua/model.py
@@ -193,6 +193,21 @@ class AquaModelApp(AquaApp):
 
         return aqua_models
 
+    def _if_show(self, model: "ModelSummary") -> bool:
+        """Determine if the given model should be return by `list`."""
+        TARGET_TAGS = model.freeform_tags.keys()
+        if not Tags.AQUA_TAG.value in TARGET_TAGS:
+            return False
+
+        return (
+            True
+            if (
+                Tags.AQUA_SERVICE_MODEL_TAG.value in TARGET_TAGS
+                or Tags.AQUA_FINE_TUNED_MODEL_TAG.value in TARGET_TAGS
+            )
+            else False
+        )
+
     def _get_artifact_path(self, custom_metadata_list: List) -> str:
         """Get the artifact path from the custom metadata list of model.
 
