7291288a27bb475abe340f84c69c407361a4e820
MING KANG <ming.kang@oracle.com>
2024-02-15 14:04:53 -0500

    added list test

--- tests/unitary/with_extras/aqua/test_model.py
+++ tests/unitary/with_extras/aqua/test_model.py
@@ -4,31 +4,132 @@
 # Copyright (c) 2024 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 
+import os
 import unittest
-from collections import namedtuple
 from dataclasses import asdict
-from unittest.mock import ANY, MagicMock, patch
+from importlib import reload
+from unittest.mock import MagicMock
 
+import oci
+
+import ads.aqua.model
+import ads.config
 from ads.aqua.model import AquaModelApp, AquaModelSummary
 
 
 class TestDataset:
-    Response = namedtuple("Response", ["data", "status"])
-    DataList = namedtuple("DataList", ["objects"])
+    model_summary_objects = [
+        {
+            "compartment_id": "ocid1.compartment.oc1..<OCID>",
+            "created_by": "ocid1.datasciencenotebooksession.oc1.iad.<OCID>",
+            "defined_tags": {},
+            "display_name": "Model1",
+            "freeform_tags": {
+                "OCI_AQUA": "",
+                "aqua_service_model": "ocid1.datasciencemodel.oc1.iad.<OCID>#Model1",
+                "license": "UPL",
+                "organization": "Oracle AI",
+                "task": "text_generation",
+            },
+            "id": "ocid1.datasciencemodel.oc1.iad.<OCID>",
+            "lifecycle_state": "ACTIVE",
+            "project_id": "ocid1.datascienceproject.oc1.iad.<OCID>",
+            "time_created": "2024-01-19T17:57:39.158000+00:00",
+        },
+    ]
 
-    resource_summary_objects = []
+    resource_summary_objects = [
+        {
+            "additional_details": {},
+            "availability_domain": "",
+            "compartment_id": "ocid1.compartment.oc1..<OCID>",
+            "defined_tags": {},
+            "display_name": "Model1-Fine-Tuned",
+            "freeform_tags": {
+                "OCI_AQUA": "",
+                "aqua_fine_tuned_model": "ocid1.datasciencemodel.oc1.iad.<OCID>#Model1",
+                "license": "UPL",
+                "organization": "Oracle AI",
+                "task": "text_generation",
+            },
+            "identifier": "ocid1.datasciencemodel.oc1.iad.<OCID>",
+            "identity_context": {},
+            "lifecycle_state": "ACTIVE",
+            "resource_type": "DataScienceModel",
+            "search_context": "",
+            "system_tags": {},
+            "time_created": "2024-01-19T19:33:58.078000+00:00",
+        },
+    ]
 
-    COMPARTMENT_ID = "ocid1.compartment.oc1..<OCID>"
-    MOCK_ICON = "data:image/svg+xml;base64,########"
+    SERVICE_COMPARTMENT_ID = "ocid1.compartment.oc1..<OCID>"
+    COMPARTMENT_ID = "ocid1.compartment.oc1..<UNIQUE_OCID>"
 
 
 class TestAquaModel(unittest.TestCase):
     """Contains unittests for AquaModelApp."""
 
-    def test_list(self):
-        """Tests list models succesfully."""
-        pass
+    @classmethod
+    def setUpClass(cls):
+        os.environ["ODSC_MODEL_COMPARTMENT_OCID"] = TestDataset.SERVICE_COMPARTMENT_ID
+        reload(ads.config)
+        reload(ads.aqua.model)
 
-    def test_list_failed(self):
-        """Tests list models succesfully."""
-        pass
+    @classmethod
+    def tearDownClass(cls):
+        os.environ.pop("ODSC_MODEL_COMPARTMENT_OCID", None)
+        reload(ads.config)
+        reload(ads.aqua.model)
+
+    def test_list_service_models(self):
+        """Tests listing service models succesfully."""
+        app = AquaModelApp()
+
+        app.list_resource = MagicMock(
+            return_value=[
+                oci.data_science.models.ModelSummary(**item)
+                for item in TestDataset.model_summary_objects
+            ]
+        )
+
+        results = app.list()
+
+        received_args = app.list_resource.call_args.kwargs
+        assert received_args.get("compartment_id") == TestDataset.SERVICE_COMPARTMENT_ID
+
+        assert len(results) == 1
+
+        attributes = AquaModelSummary.__annotations__.keys()
+        for r in results:
+            rdict = asdict(r)
+            print("############ Expected Response ############")
+            print(rdict)
+
+            for attr in attributes:
+                assert rdict.get(attr) is not None
+
+    def test_list_custom_models(self):
+        """Tests list custom models succesfully."""
+        app = AquaModelApp()
+
+        app._rqs = MagicMock(
+            return_value=[
+                oci.resource_search.models.ResourceSummary(**item)
+                for item in TestDataset.resource_summary_objects
+            ]
+        )
+
+        results = app.list(TestDataset.COMPARTMENT_ID)
+
+        app._rqs.assert_called_with(TestDataset.COMPARTMENT_ID)
+
+        assert len(results) == 1
+
+        attributes = AquaModelSummary.__annotations__.keys()
+        for r in results:
+            rdict = asdict(r)
+            print("############ Expected Response ############")
+            print(rdict)
+
+            for attr in attributes:
+                assert rdict.get(attr) is not None
