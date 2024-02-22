e37f23952676cabc1fb3b258546c3464fbabc3fb
Mayoor Rao <mayoor.rao@oracle.com>
2024-02-21 19:33:07 -0800

    Create RP when in OCI resources (#598)

--- ads/aqua/base.py
+++ ads/aqua/base.py
@@ -9,12 +9,16 @@ import oci
 from ads.common import oci_client as oc
 from ads.common.auth import default_signer
 from ads.common.utils import extract_region
+from ads.config import OCI_RESOURCE_PRINCIPAL_VERSION
+from ads import set_auth
 
 
 class AquaApp:
     """Base Aqua App to contain common components."""
 
     def __init__(self) -> None:
+        if OCI_RESOURCE_PRINCIPAL_VERSION:
+            set_auth("resource_principal")
         self._auth = default_signer()
         self.ds_client = oc.OCIClientFactory(**self._auth).data_science
         self.logging_client = oc.OCIClientFactory(**self._auth).logging_management
