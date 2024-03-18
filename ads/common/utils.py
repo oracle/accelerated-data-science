0bac26ec565f22aea23e294bb38e3dcfe08cfb75
Lu Peng <bolu.peng@oracle.com>
2024-03-18 15:41:16 -0400

    Added source filter to logging link.

--- ads/common/utils.py
+++ ads/common/utils.py
@@ -1745,10 +1745,13 @@ def get_console_link(
 def get_log_links(
     region: str,
     log_group_id: str,
+    compartment_id: str = None,
     log_id: str = None,
+    source_id: str = None,
 ) -> str:
     """
     This method returns the web console link for the given log ids.
+    
     Parameters
     ----------
     log_group_id: str, required
@@ -1757,6 +1760,10 @@ def get_log_links(
         OCID of the resource
     region: str
         The Region Identifier that the client should connect to.
+    compartment_id: str, optional
+        The compartment OCID of the resource.
+    source_id: str, optional
+        The OCID of the resource.
 
     Returns
     -------
@@ -1765,7 +1772,12 @@ def get_log_links(
     """
     console_link_url = ""
     if log_group_id and log_id:
-        console_link_url = f"https://cloud.oracle.com/logging/log-groups/{log_group_id}/logs/{log_id}?region={region}"
+        # format: https://cloud.oracle.com/logging/search?searchQuery=search "<compartment>/<log_group>/<log>" | source='<source>' | sort by datetime desc&regions=<region>
+        query_range = f'''search "{compartment_id}/{log_group_id}/{log_id}"'''
+        query_source = f"source='{source_id}'"
+        sort_condition = f"sort by datetime desc&regions={region}"
+        search_query = f"search?searchQuery={query_range} | {query_source} | {sort_condition}"
+        console_link_url = f"https://cloud.oracle.com/logging/{search_query}"
     elif log_group_id:
         console_link_url = f"https://cloud.oracle.com/logging/log-groups/{log_group_id}?region={region}"
 
