3ebac373ef72852bf845ea475dbc167deeb5657d
Vipul <vipul.mascarenhas@oracle.com>
2024-03-25 12:54:08 -0700

    handle aqua extension

--- ads/aqua/__init__.py
+++ ads/aqua/__init__.py
@@ -6,7 +6,20 @@
 
 import logging
 import sys
+import os
+from ads.aqua.utils import fetch_service_compartment
+
 
 logger = logging.getLogger(__name__)
 handler = logging.StreamHandler(sys.stdout)
 logger.setLevel(logging.INFO)
+
+
+ODSC_MODEL_COMPARTMENT_OCID = os.environ.get("ODSC_MODEL_COMPARTMENT_OCID")
+if not ODSC_MODEL_COMPARTMENT_OCID:
+    try:
+        ODSC_MODEL_COMPARTMENT_OCID = fetch_service_compartment()
+    except:
+        logger.error(
+            "ODSC_MODEL_COMPARTMENT_OCID environment variable is not set for Aqua."
+        )
