c212b0ac9f400ab76c63b8ff5718c82b544588d0
MING KANG <ming.kang@oracle.com>
2024-04-12 12:50:58 -0400

    Add test cases for handlers: Evaluation/Common/Base (#773)

--- tests/unitary/with_extras/aqua/utils.py
+++ tests/unitary/with_extras/aqua/utils.py
@@ -4,9 +4,59 @@
 # Copyright (c) 2024 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 
+from dataclasses import dataclass
 from typing import Dict
 
 from pydantic import BaseModel, PositiveInt, ValidationError
+
+
+@dataclass(repr=False)
+class MockData:
+    """Used for testing serializing dataclass in handler."""
+
+    id: str = ""
+    name: str = ""
+
+
+class HandlerTestDataset:
+    MOCK_OCID = "ocid.datasciencemdoel.<ocid>"
+    mock_valid_input = dict(
+        evaluation_source_id="ocid1.datasciencemodel.oc1.iad.<OCID>",
+        evaluation_name="test_evaluation_name",
+        dataset_path="oci://dataset_bucket@namespace/prefix/dataset.jsonl",
+        report_path="oci://report_bucket@namespace/prefix/",
+        model_parameters=dict(max_token=500),
+        shape_name="VM.Standard.E3.Flex",
+        block_storage_size=1,
+        experiment_name="test_experiment_name",
+        memory_in_gbs=1,
+        ocpus=1,
+    )
+    mock_invalid_input = dict(name="myvalue")
+    mock_dataclass_obj = MockData(id="myid", name="myname")
+    mock_service_payload_create = {
+        "target_service": "data_science",
+        "status": 404,
+        "code": "NotAuthenticated",
+        "opc-request-id": "1234",
+        "message": "The required information to complete authentication was not provided or was incorrect.",
+        "operation_name": "create_resources",
+        "timestamp": "2024-04-12T02:51:24.977404+00:00",
+        "request_endpoint": "POST xxx",
+    }
+    mock_service_payload_get = {
+        "target_service": "data_science",
+        "status": 404,
+        "code": "NotAuthenticated",
+        "opc-request-id": "1234",
+        "message": "The required information to complete authentication was not provided or was incorrect.",
+        "operation_name": "get_job_run",
+        "timestamp": "2024-04-12T02:51:24.977404+00:00",
+        "request_endpoint": "GET xxx",
+    }
+
+    def mock_url(self, action):
+        return f"{self.MOCK_OCID}/{action}"
 
 
 class SupportMetricsFormat(BaseModel):
