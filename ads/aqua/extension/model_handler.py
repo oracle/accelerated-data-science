4709211ae8bd5f025d9b4d9ae58c8403e5a59eca
Dmitrii Cherkasov <dmitrii.cherkasov@oracle.com>
2024-05-08 18:07:41 -0700

    AQUA Fetch HF Model Details

--- ads/aqua/extension/model_handler.py
+++ ads/aqua/extension/model_handler.py
@@ -3,13 +3,23 @@
 # Copyright (c) 2024 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 
+import re
+from typing import Optional
 from urllib.parse import urlparse
 
+from huggingface_hub import HfApi
+from huggingface_hub.utils import (
+    GatedRepoError,
+    HfHubHTTPError,
+    RepositoryNotFoundError,
+    RevisionNotFoundError,
+)
 from tornado.web import HTTPError
 
 from ads.aqua.decorator import handle_exceptions
-from ads.aqua.extension.base_handler import AquaAPIhandler
-from ads.aqua.model import AquaModelApp
+from ads.aqua.exception import AquaRuntimeError
+from ads.aqua.extension.base_handler import AquaAPIhandler, Errors
+from ads.aqua.model import AquaModelApp, AquaModelSummary, HFModelSummary, ModelTask
 
 
 class AquaModelHandler(AquaAPIhandler):
@@ -43,17 +53,155 @@ class AquaModelHandler(AquaAPIhandler):
         project_id = self.get_argument("project_id", default=None)
         return self.finish(AquaModelApp().list(compartment_id, project_id))
 
+
 class AquaModelLicenseHandler(AquaAPIhandler):
     """Handler for Aqua Model license REST APIs."""
 
     @handle_exceptions
     def get(self, model_id):
         """Handle GET request."""
-        
+
         model_id = model_id.split("/")[0]
         return self.finish(AquaModelApp().load_license(model_id))
+
+
+class AquaHuggingFaceHandler(AquaAPIhandler):
+    """Handler for Aqua Hugging Face REST APIs."""
+
+    def _find_matching_aqua_model(
+        self, model_id: str, author: str
+    ) -> Optional[AquaModelSummary]:
+        """
+        Finds a matching model in AQUA based on the model ID from Hugging Face.
+
+        Parameters
+        ----------
+        model_id (str): The Hugging Face model ID to match.
+        author (str): The Hugging Face model author to match.
+
+        Returns
+        -------
+        Optional[AquaModelSummary]
+            Returns the matching AquaModelSummary object if found, else None.
+        """
+        # Convert the Hugging Face model ID to lowercase once
+        model_id_lower = model_id.lower().replace(f"{author}/", "")
+        author_lower = author.lower()
+
+        compartment_id = self.get_argument("compartment_id", default=None)
+        project_id = self.get_argument("project_id", default=None)
+
+        aqua_model_app = AquaModelApp()
+        aqua_model_list = aqua_model_app.list(compartment_id, project_id)
+
+        for aqua_model_summary in aqua_model_list:
+            if (
+                aqua_model_summary.name.lower()
+                == model_id_lower
+                # and aqua_model_summary.organization.lower() == author_lower
+            ):
+                return aqua_model_summary
+
+        return None
+
+    def _format_custom_error_message(self, error: HfHubHTTPError):
+        """
+        Formats a custom error message based on the Hugging Face error response.
+
+        Parameters
+        ----------
+        error (HfHubHTTPError): The caught exception.
+
+        Returns
+        -------
+        str: A user-friendly error message.
+        """
+        # Extract the repository URL from the error message if present
+        match = re.search(r"(https://huggingface.co/[^\s]+)", str(error))
+        url = match.group(1) if match else "the requested Hugging Face URL."
+
+        if isinstance(error, RepositoryNotFoundError):
+            return (
+                f"Failed to access {url} "
+                "If the repo is private, make sure you are authenticated."
+            )
+        elif isinstance(error, GatedRepoError):
+            return (
+                f"Access denied to {url} "
+                "This repository is gated. Access is restricted to authorized users. "
+                "Please request access or check with the repository administrator. "
+                "If you are trying to access a gated repository, ensure you have a valid HF token registered. "
+                "To register your token, run this command in your terminal: `huggingface-cli login`"
+            )
+        elif isinstance(error, RevisionNotFoundError):
+            return (
+                f"The specified revision could not be found at {url} "
+                "Please check the revision identifier and try again."
+            )
+        else:
+            return (
+                f"An error occurred while accessing {url} "
+                "Please check your network connection and try again. "
+                "If you are trying to access a gated repository, ensure you have a valid HF token registered. "
+                "To register your token, run this command in your terminal: `huggingface-cli login`"
+            )
+
+    @handle_exceptions
+    def post(self, *args, **kwargs):
+        """Handles post request for the HF Models APIs
+
+        Raises
+        ------
+        HTTPError
+            Raises HTTPError if inputs are missing or are invalid.
+        """
+        try:
+            input_data = self.get_json_body()
+        except Exception:
+            raise HTTPError(400, Errors.INVALID_INPUT_DATA_FORMAT)
+
+        if not input_data:
+            raise HTTPError(400, Errors.NO_INPUT_DATA)
+
+        model_id = input_data.get("model_id")
+
+        if not model_id:
+            raise HTTPError(400, Errors.MISSING_REQUIRED_PARAMETER.format("model_id"))
+
+        # Get model info from the HF
+
+        try:
+            hf_model_info = HfApi().model_info(model_id)
+        except HfHubHTTPError as err:
+            raise AquaRuntimeError(self._format_custom_error_message(err))
+
+        # Check if model is not disabled
+        if hf_model_info.disabled:
+            raise AquaRuntimeError(
+                f"The chosen model '{hf_model_info.id}' is currently disabled and cannot be imported into AQUA. "
+                "Please verify the model's status on the Hugging Face Model Hub or select a different model."
+            )
+
+        # Check pipeline_tag, it should be `text-generation`
+        if hf_model_info.pipeline_tag.lower() != ModelTask.TEXT_GENERATION.value:
+            raise AquaRuntimeError(
+                f"Unsupported pipeline tag for the chosen model: '{hf_model_info.pipeline_tag}'. "
+                f"AQUA currently supports the following tasks only: {', '.join(ModelTask.values())}. "
+                "Please select a model with a compatible pipeline tag."
+            )
+
+        # Check if it is a service/shadow model
+        aqua_model_info: AquaModelSummary = self._find_matching_aqua_model(
+            model_id=hf_model_info.id, author=hf_model_info.author
+        )
+
+        return self.finish(
+            HFModelSummary(model_info=hf_model_info, aqua_model_info=aqua_model_info)
+        )
+
 
 __handlers__ = [
     ("model/?([^/]*)", AquaModelHandler),
     ("model/?([^/]*)/license", AquaModelLicenseHandler),
+    ("model/hf/search/?([^/]*)", AquaHuggingFaceHandler),
 ]
--- tests/unitary/with_extras/aqua/test_model_handler.py
+++ tests/unitary/with_extras/aqua/test_model_handler.py
@@ -5,17 +5,23 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 
 from unittest import TestCase
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 
-from mock import patch
+import pytest
+from huggingface_hub import HfApi, hf_api
+from huggingface_hub.utils import GatedRepoError
 from notebook.base.handlers import IPythonHandler
+from tornado.web import HTTPError
 
-from ads.aqua.extension.model_handler import AquaModelHandler, AquaModelLicenseHandler
-from ads.aqua.model import AquaModelApp
+from ads.aqua.extension.model_handler import (
+    AquaHuggingFaceHandler,
+    AquaModelHandler,
+    AquaModelLicenseHandler,
+)
+from ads.aqua.model import AquaModelApp, AquaModelSummary, HFModelSummary
 
 
 class ModelHandlerTestCase(TestCase):
-    
     @patch.object(IPythonHandler, "__init__")
     def setUp(self, ipython_init_mock) -> None:
         ipython_init_mock.return_value = None
@@ -36,9 +42,7 @@ class ModelHandlerTestCase(TestCase):
     @patch.object(AquaModelApp, "get")
     def test_read(self, mock_get):
         self.model_handler.read(model_id="test_model_id")
-        self.model_handler.finish.assert_called_with(
-            mock_get.return_value
-        )
+        self.model_handler.finish.assert_called_with(mock_get.return_value)
         mock_get.assert_called_with("test_model_id")
 
     @patch.object(AquaModelApp, "clear_model_list_cache")
@@ -59,13 +63,11 @@ class ModelHandlerTestCase(TestCase):
     def test_list(self, mock_list):
         self.model_handler.list()
 
-        self.model_handler.finish.assert_called_with(
-            mock_list.return_value
-        )
+        self.model_handler.finish.assert_called_with(mock_list.return_value)
         mock_list.assert_called_with(None, None)
 
+
 class ModelLicenseHandlerTestCase(TestCase):
-    
     @patch.object(IPythonHandler, "__init__")
     def setUp(self, ipython_init_mock) -> None:
         ipython_init_mock.return_value = None
@@ -80,3 +82,156 @@ class ModelLicenseHandlerTestCase(TestCase):
             mock_load_license.return_value
         )
         mock_load_license.assert_called_with("test_model_id")
+
+
+class TestAquaHuggingFaceHandler:
+    def setup_method(self):
+        with patch.object(IPythonHandler, "__init__"):
+            self.mock_handler = AquaHuggingFaceHandler(MagicMock(), MagicMock())
+            self.mock_handler.request = MagicMock()
+            self.mock_handler.finish = MagicMock()
+            self.mock_handler.set_header = MagicMock()
+            self.mock_handler.set_status = MagicMock()
+
+    @pytest.mark.parametrize(
+        "test_model_id, test_author, expected_aqua_model_name",
+        [
+            ("organization1/name1", "organization1", "name1"),
+            ("organization1/name2", "organization1", "name2"),
+            ("organization2/name3", "organization2", "name3"),
+            ("organization1/name3", "organization1", "name3"),
+            ("name3", "organization2", "name3"),
+            ("organization1/non_existing_name", "organization1", None),
+        ],
+    )
+    def test_find_matching_aqua_model(
+        self, test_model_id, test_author, expected_aqua_model_name
+    ):
+        with patch.object(AquaModelApp, "list") as aqua_model_mock_list:
+            aqua_model_mock_list.return_value = [
+                AquaModelSummary(name="name1", organization="organization1"),
+                AquaModelSummary(name="name2", organization="organization1"),
+                AquaModelSummary(name="name3", organization="organization2"),
+            ]
+
+            test_result = self.mock_handler._find_matching_aqua_model(
+                model_id=test_model_id, author=test_author
+            )
+
+            aqua_model_mock_list.assert_called_once()
+
+            if expected_aqua_model_name:
+                assert test_result.name == expected_aqua_model_name
+            else:
+                assert test_result == None
+
+    @patch("uuid.uuid4")
+    def test_post_negative(self, mock_uuid):
+        mock_uuid.return_value = "###"
+
+        # case 1
+        self.mock_handler.get_json_body = MagicMock(side_effect=ValueError())
+        self.mock_handler.post()
+        self.mock_handler.finish.assert_called_with(
+            '{"status": 400, "message": "Invalid format of input data.", "service_payload": {}, "reason": "Invalid format of input data.", "request_id": "###"}'
+        )
+
+        # case 2
+        self.mock_handler.get_json_body = MagicMock(return_value={})
+        self.mock_handler.post()
+        self.mock_handler.finish.assert_called_with(
+            '{"status": 400, "message": "No input data provided.", "service_payload": {}, '
+            '"reason": "No input data provided.", "request_id": "###"}'
+        )
+
+        # case 3
+        self.mock_handler.get_json_body = MagicMock(return_value={"some_field": None})
+        self.mock_handler.post()
+        self.mock_handler.finish.assert_called_with(
+            '{"status": 400, "message": "Missing required parameter: \'model_id\'", '
+            '"service_payload": {}, "reason": "Missing required parameter: \'model_id\'", "request_id": "###"}'
+        )
+
+        # case 4
+        self.mock_handler.get_json_body = MagicMock(
+            return_value={"model_id": "test_model_id"}
+        )
+        self.mock_handler._format_custom_error_message = MagicMock(
+            return_value="test error message"
+        )
+        with patch.object(HfApi, "model_info") as mock_model_info:
+            mock_model_info.side_effect = GatedRepoError(message="test message")
+            self.mock_handler.post()
+            self.mock_handler.finish.assert_called_with(
+                '{"status": 400, "message": "Something went wrong with your request.", '
+                '"service_payload": {}, "reason": "test error message", "request_id": "###"}'
+            )
+
+        # case 5
+        self.mock_handler.get_json_body = MagicMock(
+            return_value={"model_id": "test_model_id"}
+        )
+        with patch.object(HfApi, "model_info") as mock_model_info:
+            mock_model_info.return_value = MagicMock(disabled=True, id="test_model_id")
+            self.mock_handler.post()
+            self.mock_handler.finish.assert_called_with(
+                '{"status": 400, "message": "Something went wrong with your request.", "service_payload": {}, '
+                '"reason": "The chosen model \'test_model_id\' is currently disabled and cannot be '
+                "imported into AQUA. Please verify the model's status on the Hugging Face Model "
+                'Hub or select a different model.", "request_id": "###"}'
+            )
+
+        # case 6
+        self.mock_handler.get_json_body = MagicMock(
+            return_value={"model_id": "test_model_id"}
+        )
+        with patch.object(HfApi, "model_info") as mock_model_info:
+            mock_model_info.return_value = MagicMock(
+                disabled=False, id="test_model_id", pipeline_tag="not-text-generation"
+            )
+            self.mock_handler.post()
+            self.mock_handler.finish.assert_called_with(
+                '{"status": 400, "message": "Something went wrong with your request.", '
+                '"service_payload": {}, "reason": "Unsupported pipeline tag for the chosen '
+                "model: 'not-text-generation'. AQUA currently supports the following tasks only: "
+                'text-generation. Please select a model with a compatible pipeline tag.", "request_id": "###"}'
+            )
+
+    @patch("uuid.uuid4")
+    def test_post_positive(self, mock_uuid):
+        mock_uuid.return_value = "###"
+
+        self.mock_handler.get_json_body = MagicMock(
+            return_value={"model_id": "test_model_id"}
+        )
+
+        test_aqua_model_summary = AquaModelSummary(
+            name="name1", organization="organization1"
+        )
+        self.mock_handler._find_matching_aqua_model = MagicMock(
+            return_value=test_aqua_model_summary
+        )
+
+        with patch.object(HfApi, "model_info") as mock_model_info:
+            test_hf_model_info = hf_api.ModelInfo(
+                disabled=False,
+                id="test_model_id",
+                pipeline_tag="text-generation",
+                author="test_author",
+                private=False,
+                downloads=10,
+                likes=10,
+                tags=None,
+            )
+            mock_model_info.return_value = test_hf_model_info
+            self.mock_handler.post()
+
+            self.mock_handler._find_matching_aqua_model.assert_called_with(
+                model_id="test_model_id", author="test_author"
+            )
+
+            test_model_summary = HFModelSummary(
+                model_info=test_hf_model_info, aqua_model_info=test_aqua_model_summary
+            )
+
+            self.mock_handler.finish.assert_called_with(test_model_summary)
