a1966581c9f26ea23eae2aed92e496c9c4a4e896
lrudenka <liuda.rudenka@oracle.com>
2024-07-16 17:22:00 -0500

    add logger.info

--- ads/aqua/common/utils.py
+++ ads/aqua/common/utils.py
@@ -193,6 +193,7 @@ def read_file(file_path: str, **kwargs) -> str:
             return f.read()
     except Exception as e:
         logger.debug(f"Failed to read file {file_path}. {e}")
+        logger.info(f"DELETE THIS. file_path: {file_path}, kwargs: {kwargs}")
         logger.exception(e)
         return UNKNOWN
 
