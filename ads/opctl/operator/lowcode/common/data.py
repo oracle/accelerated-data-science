8134de955343624f48e57d62459dcca76d21f829
Allen <allen.hosler@oracle.com>
2024-11-12 12:09:05 +0000

    update data file

--- ads/opctl/operator/lowcode/common/data.py
+++ ads/opctl/operator/lowcode/common/data.py
@@ -1,29 +1,28 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*--
 
 # Copyright (c) 2024 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 
 import time
-from .transformations import Transformations
+from abc import ABC, abstractmethod
+
+import pandas as pd
+
 from ads.opctl import logger
 from ads.opctl.operator.lowcode.common.const import DataColumns
-from ads.opctl.operator.lowcode.common.utils import load_data
 from ads.opctl.operator.lowcode.common.errors import (
-    InputDataError,
     InvalidParameterError,
-    PermissionsError,
-    DataMismatchError,
 )
-from abc import ABC
-import pandas as pd
+from ads.opctl.operator.lowcode.common.utils import load_data
+
+from .transformations import Transformations
 
 
 class AbstractData(ABC):
     def __init__(self, spec: dict, name="input_data"):
         self.Transformations = Transformations
         self.data = None
-        self._data_dict = dict()
+        self._data_dict = {}
         self.name = name
         self.spec = spec
         self.load_transform_ingest_data(spec)
@@ -35,11 +34,14 @@ class AbstractData(ABC):
         condition = pd.Series(True, index=self.raw_data.index)
         if category in mapping:
             for col, val in mapping[category].items():
-                condition &= (self.raw_data[col] == val)
+                condition &= self.raw_data[col] == val
         data_by_cat = self.raw_data[condition].reset_index(drop=True)
-        data_by_cat = self._data_transformer._format_datetime_col(data_by_cat) if self.spec.datetime_column else data_by_cat
+        data_by_cat = (
+            self._data_transformer._format_datetime_col(data_by_cat)
+            if self.spec.datetime_column
+            else data_by_cat
+        )
         return data_by_cat
-
 
     def get_dict_by_series(self):
         if not self._data_dict:
@@ -59,12 +61,12 @@ class AbstractData(ABC):
         data_dict = self.get_dict_by_series()
         try:
             return data_dict[series_id]
-        except:
+        except Exception as e:
             raise InvalidParameterError(
                 f"Unable to retrieve series {series_id} from {self.name}. Available series ids are: {self.list_series_ids()}"
-            )
+            ) from e
 
-    def _load_data(self, data_spec, **kwargs):
+    def _load_data(self, data_spec):
         loading_start_time = time.time()
         try:
             raw_data = load_data(data_spec)
@@ -77,7 +79,7 @@ class AbstractData(ABC):
         )
         return raw_data
 
-    def _transform_data(self, spec, raw_data, **kwargs):
+    def _transform_data(self, spec, raw_data):
         transformation_start_time = time.time()
         self._data_transformer = self.Transformations(spec, name=self.name)
         data = self._data_transformer.run(raw_data)
@@ -92,6 +94,7 @@ class AbstractData(ABC):
         self.data = self._transform_data(spec, self.raw_data)
         self._ingest_data(spec)
 
+    @abstractmethod
     def _ingest_data(self, spec):
         pass
 
