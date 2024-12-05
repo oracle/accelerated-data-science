2aa919addd0e24a932a932df204455a300d83366
Qiu Qin <qiu.qin@oracle.com>
2024-12-05 12:09:09 -0500

    Catch logging exception and log traceback.

--- ads/llm/autogen/v02/runtime_logging.py
+++ ads/llm/autogen/v02/runtime_logging.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2024, Oracle and/or its affiliates.  All rights reserved.
 # This software is dual-licensed to you under the Universal Permissive License (UPL) 1.0 as shown at https://oss.oracle.com/licenses/upl or Apache License 2.0 as shown at http://www.apache.org/licenses/LICENSE-2.0. You may choose either license.
 import logging
+import traceback
 from sqlite3 import Connection
 from typing import Any, Dict, List, Optional
 
@@ -8,10 +9,11 @@ import autogen.runtime_logging
 from autogen.logger.base_logger import BaseLogger
 from autogen.logger.logger_factory import LoggerFactory
 
-module_logger = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
 
 class LoggerManager(BaseLogger):
+    """Manages multiple AutoGen loggers."""
 
     def __init__(self) -> None:
         self.loggers: List[BaseLogger] = []
@@ -21,36 +23,47 @@ class LoggerManager(BaseLogger):
         """Adds a new AutoGen logger."""
         self.loggers.append(logger)
 
-    def call_loggers(self, method, *args, **kwargs) -> None:
+    def _call_loggers(self, method: str, *args, **kwargs) -> None:
+        """Calls the specific method on each AutoGen logger in self.loggers."""
         for autogen_logger in self.loggers:
-            getattr(autogen_logger, method)(*args, **kwargs)
+            try:
+                getattr(autogen_logger, method)(*args, **kwargs)
+            except Exception as e:
+                # Catch the logging exception so that the program will not be interrupted.
+                logger.error(
+                    "Failed to %s with %s: %s",
+                    method,
+                    autogen_logger.__class__.__name__,
+                    str(e),
+                )
+                logger.debug(traceback.format_exc())
 
     def start(self) -> str:
-        return self.call_loggers("start")
+        return self._call_loggers("start")
 
     def stop(self) -> None:
-        return self.call_loggers("stop")
+        return self._call_loggers("stop")
 
     def get_connection(self) -> None | Connection:
-        return self.call_loggers("get_connection")
+        return self._call_loggers("get_connection")
 
     def log_chat_completion(self, *args, **kwargs) -> None:
-        return self.call_loggers("log_chat_completion", *args, **kwargs)
+        return self._call_loggers("log_chat_completion", *args, **kwargs)
 
     def log_new_agent(self, *args, **kwargs) -> None:
-        return self.call_loggers("log_new_agent", *args, **kwargs)
+        return self._call_loggers("log_new_agent", *args, **kwargs)
 
     def log_event(self, *args, **kwargs) -> None:
-        return self.call_loggers("log_event", *args, **kwargs)
+        return self._call_loggers("log_event", *args, **kwargs)
 
     def log_new_wrapper(self, *args, **kwargs) -> None:
-        return self.call_loggers("log_new_wrapper", *args, **kwargs)
+        return self._call_loggers("log_new_wrapper", *args, **kwargs)
 
     def log_new_client(self, *args, **kwargs) -> None:
-        return self.call_loggers("log_new_client", *args, **kwargs)
+        return self._call_loggers("log_new_client", *args, **kwargs)
 
     def log_function_use(self, *args, **kwargs) -> None:
-        return self.call_loggers("log_function_use", *args, **kwargs)
+        return self._call_loggers("log_function_use", *args, **kwargs)
 
     def __repr__(self) -> str:
         return "\n\n".join(
@@ -62,35 +75,51 @@ class LoggerManager(BaseLogger):
 
 
 def start(
-    logger: Optional[BaseLogger] = None,
+    autogen_logger: Optional[BaseLogger] = None,
     logger_type: str = None,
     config: Optional[Dict[str, Any]] = None,
 ) -> str:
-    if logger and logger_type:
+    """Starts logging with AutoGen logger.
+    Specify your custom autogen_logger, or the logger_type and config to use a built-in logger.
+
+    Parameters
+    ----------
+    autogen_logger : BaseLogger, optional
+        An AutoGen logger, which should be a subclass of autogen.logger.base_logger.BaseLogger.
+    logger_type : str, optional
+        Logger type of a built-in AutoGen logger (for example, "file"), by default None.
+    config : dict, optional
+        Configurations for the built-in AutoGen logger, by default None
+
+    Returns
+    -------
+    str
+        A unique session ID returned from starting the logger.
+
+    """
+    if autogen_logger and logger_type:
         raise ValueError(
-            "Please specify only logger(%s) or logger_type(%s).", logger, logger_type
+            "Please specify only autogen_logger(%s) or logger_type(%s).",
+            autogen_logger,
+            logger_type,
         )
 
     # Check if a logger is already configured
-    autogen_logger = autogen.runtime_logging.autogen_logger
-    if not autogen_logger:
+    existing_logger = autogen.runtime_logging.autogen_logger
+    if not existing_logger:
         # No logger is configured
         logger_manager = LoggerManager()
-    elif not isinstance(autogen_logger, LoggerManager):
-        # Logger is configured but it is not via ADS
-        module_logger.warning(
-            "AutoGen is already configured with %s", str(autogen_logger)
-        )
-        logger_manager = LoggerManager()
-        logger_manager.add_logger(autogen_logger)
-    else:
+    elif isinstance(existing_logger, LoggerManager):
         # Logger is already configured with ADS
-        logger_manager = autogen_logger
+        logger_manager = existing_logger
+    else:
+        # Logger is configured but it is not via ADS
+        logger.warning("AutoGen is already configured with %s", str(existing_logger))
+        logger_manager = LoggerManager()
+        logger_manager.add_logger(existing_logger)
 
     # Add AutoGen logger
-    if logger:
-        autogen_logger = logger
-    else:
+    if not autogen_logger:
         autogen_logger = LoggerFactory.get_logger(
             logger_type=logger_type, config=config
         )
@@ -102,8 +131,7 @@ def start(
         autogen.runtime_logging.autogen_logger = logger_manager
     except Exception as e:
         logger.error(f"Failed to start logging: {e}")
-    finally:
-        return session_id
+    return session_id
 
 
 def stop() -> BaseLogger:
