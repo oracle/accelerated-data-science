a1ae2be43fdac542b505c7f4783bb0b7f6e9c59d
Lu Peng <bolu.peng@oracle.com>
2024-12-10 21:21:37 -0500

    Updated pr.

--- tests/unitary/with_extras/hpo/customized_scoring.py
+++ tests/unitary/with_extras/hpo/customized_scoring.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 
-# Copyright (c) 2021, 2023 Oracle and/or its affiliates.
+# Copyright (c) 2021, 2024 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 
 import numpy as np
 from sklearn.metrics import make_scorer
+
+import pytest
+
+pytest.skip(allow_module_level=True)
 
 
 def customerize_score(y_true, y_pred, sample_weight=None):
