9750e0639c15579e7f953fb6bb78b77ce01b2b31
Qiu Qin <qiu.qin@oracle.com>
2024-12-17 14:34:05 -0500

    Update copyrights.

--- ads/llm/autogen/v02/client.py
+++ ads/llm/autogen/v02/client.py
@@ -1,5 +1,5 @@
 # Copyright (c) 2024, Oracle and/or its affiliates.  All rights reserved.
-# This software is dual-licensed to you under the Universal Permissive License (UPL) 1.0 as shown at https://oss.oracle.com/licenses/upl or Apache License 2.0 as shown at http://www.apache.org/licenses/LICENSE-2.0. You may choose either license.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 
 """This module contains the custom LLM client for AutoGen v0.2 to use LangChain chat models.
 https://microsoft.github.io/autogen/0.2/blog/2024/01/26/Custom-Models/
