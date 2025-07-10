fcfd0b8cf8c1390e5a036a87f487d257d00efc95
Kumar Ranjan <kumar.s.ranjan@oracle.com>
2025-07-10 15:16:54 +0530

    Fixing MD list call

--- ads/aqua/modeldeployment/deployment.py
+++ ads/aqua/modeldeployment/deployment.py
@@ -864,17 +864,21 @@ class AquaDeploymentApp(AquaApp):
             if oci_aqua:
                 # skipping the AQUA model deployments that are created from model group
                 # TODO: remove this checker after AQUA deployment is integrated with model group
+
                 aqua_model_id = model_deployment.freeform_tags.get(
                     Tags.AQUA_MODEL_ID_TAG, UNKNOWN
                 )
-                if "datasciencemodelgroup" in aqua_model_id or not aqua_model_id:
+                if (
+                    "datasciencemodelgroup" in aqua_model_id
+                    or model_deployment.model_deployment_configuration_details.deployment_type
+                    == "UNKNOWN_ENUM_VALUE"
+                ):
                     continue
                 results.append(
                     AquaDeployment.from_oci_model_deployment(
                         model_deployment, self.region
                     )
                 )
-
                 # log telemetry if MD is in active or failed state
                 deployment_id = model_deployment.id
                 state = model_deployment.lifecycle_state.upper()
