8942499dc44795f3dafaff59d5d2c1da6014d0ec
Agrim Khanna <agrim.khanna@oracle.com>
2025-07-30 14:32:06 +0530

    fixed method name

--- ads/aqua/extension/deployment_handler.py
+++ ads/aqua/extension/deployment_handler.py
@@ -395,10 +395,10 @@ class AquaModelListHandler(AquaAPIhandler):
         self.set_header("Content-Type", "application/json")
         endpoint: str = ""
         model_deployment = AquaDeploymentApp().get(model_deployment_id)
-        endpoint = model_deployment.endpoint.rstrip("/") + "/predict/v1/models"       
+        endpoint = model_deployment.endpoint.rstrip("/") + "/predict/v1/models"
         aqua_client = Client(endpoint=endpoint)
         try:
-            list_model_result = aqua_client.list_models()
+            list_model_result = aqua_client.fetch_data()
             return self.finish(list_model_result)
         except Exception as ex:
             raise HTTPError(500, str(ex))
