79085d79b2deeb7042635111a0abf4c813982e8b
Prashant Sankhla <prashant.s.sankhla@oracle.com>
2026-05-04 17:14:07 +0530

    Update documentation; fix failing tests

--- ads/opctl/operator/lowcode/forecast/model/meta_features.py
+++ ads/opctl/operator/lowcode/forecast/model/meta_features.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright (c) 2025 Oracle and/or its affiliates.
+# Copyright (c) 2026 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 
 """Utilities to compute rule-based meta-features for forecast model selection."""
